04363ae14eaf1f10197ac4a18ee5a7332b0f40f4
Gábor Hojtsy <gabor@hojtsy.hu>
2009-02-11 20:35:09 +0000

    #372919 by Aaron Stanush: box should be narrow-box

--- drupalorg/drupalorg-start.tpl.php
+++ drupalorg/drupalorg-start.tpl.php
@@ -5,7 +5,7 @@
   <a href="forum"><img src="sites/all/themes/bluecheese/images/gettingstarted-header4.png" alt="Get Support"/></a>
 </div>
 <div class="grid-3 alpha">
-  <div class="box">
+  <div class="narrow-box">
     <p>The Drupal Core is a collection of files that you must download in order to get started with Drupal. </p>
     <a class="link-button" href="#"><span>Download Drupal <?php print $version ?></span></a>
     <h5>Installation Profiles</h5>
@@ -19,19 +19,19 @@
   </div>
 </div>
 <div class="grid-3">
-  <div class="box">
+  <div class="narrow-box">
     <p>Access hundreds of modules to customize and extend your site. </p>
-      <div class="box-list">
+      <div class="narrow-box-list">
         <h5 class="lined">Most Popular Modules</h5>
         <?php print $most_popular_modules; ?>
         <a href="#" class="all">All modules</a>
-      </div><!--/box-list-->
-      <div class="box-list">
+      </div><!--/narrow-box-list-->
+      <div class="narrow-box-list">
         <h5 class="lined">Most Popular Themes</h5>
         <?php print $most_popular_themes; ?>
         <a href="#" class="all">All themes</a>
-      </div><!--/box-list-->
-      <div class="box-list">
+      </div><!--/narrow-box-list-->
+      <div class="narrow-box-list">
         <h5 class="lined">Translations</h5>
         <ul>
           <li><a href="#">Spanish</a></li>
@@ -39,13 +39,13 @@
           <li><a href="#">German</a></li>
         </ul>
         <a href="#" class="all">All translations</a>
-    </div><!--/box-list-->
+    </div><!--/narrow-box-list-->
   </div>
 </div>
 <div class="grid-3">
-  <div class="box">
+  <div class="narrow-box">
     <p>Want more information before you begin using Drupal? Our extensive documentation will tell you how.</p>
-    <div class="box-list">
+    <div class="narrow-box-list">
       <h5 class="lined">Handbooks</h5>
       <ul>
         <li><a href="#">Installation Instructions</a></li>
@@ -55,16 +55,16 @@
         <li><a href="#">Adding Content to Your Site</a></li>
       </ul>
       <a href="#" class="all">All Handbooks</a>
-    </div><!--/box-list-->
-    <div class="box-list">
+    </div><!--/narrow-box-list-->
+    <div class="narrow-box-list">
       <h5 class="lined">Recent Books and Resources</h5>
       <p><img src="images/book_dd.jpg" height="72"><a href="#">Pro Drupal Development</a><br>
       Drupal is one of the most popular content management systems in use today. With it, you can create a variety of community-driven sites, including blogs, forums, wiki-style sites, and much more.</p>
-    </div><!--/box-list-->
+    </div><!--/narrow-box-list-->
   </div>
 </div>
 <div class="grid-3 omega">
-  <div class="box">
+  <div class="narrow-box">
     <p>Drupal's community offers a wealth of support. See more resources on our <a href="#">Community &amp; Support</a> page.</p>
     <h5 class="lined"><a href="#">Forum</a></h5>
     <p>Whether you are asking questions or answering them, the forum will connect you to the community and provide the help you need.</p>
