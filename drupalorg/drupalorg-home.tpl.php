eec73ee4c22a2b671b5a28e816ae4212fd907ae1
Gábor Hojtsy <gabor@hojtsy.hu>
2009-02-12 17:18:19 +0000

    Home page template for filling up with dynamic data

---
+++ drupalorg/drupalorg-home.tpl.php
@@ -0,0 +1,75 @@
+<div id="front-top-left" class="grid-4 alpha">
+  <div id="front-top-left-inner">
+   <h2><a href="#">Why Choose Drupal?</a></h2>
+   <p>Drupal is like Lego. Connect the pieces and build a site limited only by your imagination. Drupal's passionate, vibrant community are always creating new pieces, or improving existing ones. Choosing Drupal means as your needs evolve, so does your site.</p>
+   <p class="front-get-started">
+    <a href="#" class="link-button"><span>Get Started with Drupal</span></a>
+   </p>
+   <h4><a href="#">Who Else Uses Drupal?</a></h4>
+   Magazines: <a href="#">Fast Company</a>, <a href="#">Popular Science</a>.
+   Newspapers:<a href="#">The Onion</a>, <a href="#">Morris Digital</a>, <a href="#">Seattle Times</a> and many, many <a href="#">more...</a>
+  </div>
+</div>
+
+<div id="front-top-middle" class="grid-4">
+  <div id="front-top-middle-inner">
+   <h2><a href="#">Things we made with Drupal</a></h2>
+   <img src="/files/mtv_screenshot.jpg" class="homeimage" />
+  </div> 
+</div> 
+
+<div id="front-top-right" class="grid-4 omega">
+  <div id="front-top-right-inner">
+   <h2><a href="#">Develop with Drupal</a></h2>
+   <p>Drupal is extensible, powerful, scalable, and flexible.</p>
+   <div class="grid-2 alpha">
+    <h4><a href="#">Current activity</a></h4>
+    <ul>
+      <li><a href="#">4212 CVS a/c holders</a></li>
+      <li><a href="#">612 commits this month</a></li>
+    </ul>
+   </div>
+   <div class="grid-2 omega">
+    <ul>
+      <li><a href="#">Drupal API</a></li>
+      <li><a href="#">Download Drupal</a></li>
+      <li><a href="#">Security Info</a></li>
+      <li><a href="#">Issue List</a></li>
+    </ul>
+   </div>
+   <h4><a href="#">Modules and Themes</a></h4>
+   Explore Drupal <a href="#">modules</a> and <a href="#">themes</a>
+   <div class="advertising">
+    <img src="/files/ad.png"/>
+    <p><a href="#">Advertising</a> helps build a successful ecosystem around Drupal</p>
+   </div>
+  </div>
+</div>
+
+<div id="front-middle" class="grid-12 alpha omega">
+  <div id="front-middle-inner">
+    <div id="front-drupal-stats"><span class="blue">310,721</span> people in <span class="blue">24</span> countries speaking <span class="blue">14</span> different languages are using Drupal.</div>
+  </div>
+</div>
+
+<div id="front-bottom-left" class="grid-6 alpha">
+  <div id="front-bottom-left-inner">
+   <object width="450" height="250" id="myMovieName" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
+    <param value="http://drupal.markboultondesign.com/iteration11/images/flash/map.swf" name="movie"/>
+    <param value="high" name="quality"/>
+    <param value="#FFFFFF" name="bgcolor"/>
+    <embed width="450" height="250" align="" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="myMovieName" bgcolor="#FFFFFF" quality="high" src="http://drupal.markboultondesign.com/iteration11/images/flash/map.swf"/>
+   </object>
+  </div>
+</div>
+
+<div id="front-bottom-right" class="grid-6 omega">
+  <div id="front-bottom-right-inner">
+    <p>New Drupal Book Published: Drupal Multimedia</p>
+    <p>September 18, 2008</p>
+    <p>A year after answering Dries Buytaert's call for Drupal book authors, Aaron Winborn is pleased to announce the publication of Drupal Multimedia, which is now available! Read more</p>
+    <p>Drupal Wins Best Overall 2008 Open Source CMS Award for Second Year in a Row</p>
+    <p>Drupal wins 2008 Best PHP Open Source CMS from Packt Publishing</p>
+    <p>More news...</p>
+  </div>
+</div>
