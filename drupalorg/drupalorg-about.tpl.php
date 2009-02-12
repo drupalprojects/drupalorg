0682f328d4f83f86840244cc7de07a4a014284a9
Gábor Hojtsy <gabor@hojtsy.hu>
2009-02-12 18:37:21 +0000

    #373475 by mikejoconner: About page

---
+++ drupalorg/drupalorg-about.tpl.php
@@ -0,0 +1,124 @@
+<div class="node-section grid-12 alpha omega">
+  <div class="grid-3 alpha">
+    <h2>Build for Today</h2>
+    <p>Drupal is <a href="#">powerful, customizable, and flexible</a>, letting you connect many pieces to make your website. As your site evolves you simply move the pieces, take some away, or add new ones. The site you need now may not be the site you need in the future, so you can extend your site with more pieces.</p>
+  </div>
+      
+  <div class="grid-3">
+    <h2>Community Support</h2>
+    <p>The passionate <a href="#">Drupal community</a> is on hand to give support via various <a href="#">vibrant IRC channels</a>, in the <a href="#">forums</a>, and face to face at <a href="#">Drupal events</a>. As Drupal is <a href="#">open source</a>, this means everybody can <a href="#">get involved</a> and make a difference.</p>
+  </div>
+      
+  <div class="grid-3">
+    <h2>Dedicated Security Team</h2>
+    <p><a href="#">We take security seriously</a> and have a <a href="#">dedicated team</a> that has a <a href="#">proven track record</a> in dealing with your <a href="#">security issues</a> professionally and efficiently, meaning you can spend more time creating your site.</p>
+  </div>
+      
+  <div class="grid-3 omega">
+    <h2>Commercial Ecosystem</h2>
+    <p>As well as the community, there are <a href="#">many dedicated companies</a> in the <a href="#">Marketplace</a> on hand to help with your Drupal project. Providing expertise and a deeper understanding, they can help with <a href="#">hosting</a>, <a href="#">spam blocking</a>, <a href="#">training</a>, <a href="#">and more.</a></p>
+  </div>
+</div>
+
+<div class="node-section grid-12 alpha omega" id="about-features">
+  <h2>Drupal Features</h2>
+  
+  <div class="node-sub-section grid-12 alpha omega">
+    <div class="grid-3 alpha">
+      <h3 id="aboutwebbased"><a href="#">WEB-BASED ADMINISTRATION</a></h3>
+      <p>Drupal can be updated and <a href="#">managed using a web browser</a> so you can access it from around the world without the need for additional software.</p>
+    </div>
+      
+    <div class="grid-3">
+      <h3 id="aboutcontentmanagement"><a href="#">CONTENT MANAGEMENT</a></h3>
+      <p>Drupal comes with <a href="#">polls</a>, <a href="#">templating</a>, <a href="#">threaded comments</a>, and <a href="#">version control</a>. These features allow you to manage your site's content and let users interact with your content.</p>
+    </div>
+      
+    <div class="grid-3">
+      <h3 id="aboutmodules"><a href="#">MODULES</a></h3>
+      <p>Whether you need a <a href="#">gallery</a>, <a href="#">contact form</a>, or <a href="#">shopping cart</a>, <a href="#">Drupal modules</a> let you build, customize, and extend your site as much as you want. You can even <a href="#">contribute modules</a> for others to use.</p>
+    </div>
+      
+    <div class="grid-3 omega">
+      <h3 id="aboutthemes"><a href="#">THEMES</a></h3>
+      <p>Use an existing <a href="#">Drupal theme</a> for your site, giving it an instant identity! If you can't find one you like, try designing your own which others can use, too.</p>
+    </div>
+  </div>
+  
+  <div class="node-sub-section grid-12 alpha omega">
+    <div class="grid-3 alpha">
+      <h3 id="aboutsearching"><a href="#">SEARCHING</a></h3>
+      <p>Spend less time looking for answers and more time creating your space on the web. Our search function does the hard work for you, giving you solutions at the click of a button.</p>
+    </div>
+      
+    <div class="grid-3">
+      <h3 id="aboutsocial"><a href="#">SOCIAL PUBLISHING</a></h3>
+      <p>Drupal allows you to create sites and applications to help people express their opinions and engage with one another through <a href="#">Wikis</a>, <a href="#">Forums</a>, and <a href="#">Blogs</a></p>
+    </div>
+      
+    <div class="grid-3">
+      <h3 id="aboutcommunity"><a href="#">COMMUNITY SUPPORT</a></h3>
+      <p>Drupal has a <a href="#">worldwide community</a> that is one hand to <a href="#">help with your Drupal problems</a>, <a href="#">share ideas</a>, and even meet for coffee at a local <a href="#">Drupal event.</a></p>
+    </div>
+      
+    <div class="grid-3 omega">
+      <h3 id="aboutpersonalisation"><a href="#">PERSONALIZATION</a></h3>
+      <p>Make your Drupal site stand out. Add galleries, create themes, extend it with modules and let your imagination run wild.</p>
+    </div>
+  </div>
+</div>
+
+<div class="node-footer-section grid-12 alpha omega">
+  <div class="grid-4 alpha">
+    <h3>Who's Using Drupal</h3>
+    <div class="aboutsmall">
+      <p>From local businesses to global corporations, diverse organizations use Drupal.</p>
+      <dl>
+        <dt>News Publishing</dt>
+        <dd><a href="#">New York Observer</a>, <a href="#">NowPublic</a>, <a href="#">Popular Science</a></dd>
+  
+        <dt>Intranet/Corporate Websites</dt>
+        <dd><a href="#">Yahoo! Research</a>, <a href="#">AOL Corporate</a></dd>
+        
+        <dt>Education</dt>
+        <dd><a href="#">Amherst College</a>, <a href="#">e-Learning Institute</a>, <a href="#">WPA</a></dd>
+
+        <dt>Art, Music, Multimedia</dt>
+        <dd><a href="#">MTV UK</a>, <a href="#">Sony Music</a>, <a href="#">Warner Brothers Records</a></dd>
+      
+        <dt>Community Portal Sites</dt>
+        <dd><a href="#">Fast Company</a>, <a href="#">Team Sugar</a>, <a href="#">Kerneltrap</a></dd>
+
+        <dt>Social Networking Sites</dt>
+        <dd><a href="#">DrupalSN</a>, <a href="#">imbee</a>, <a href="#">GoingOn</a></dd>
+      </dl>
+    </div>
+  </div>
+      
+  <div class="grid-4">
+    <h3>Download a One-Page Brochure</h3>
+    <div class="aboutsmall">
+      <p>Short on time? Decision-maker too busy to look at this site? <a href="#">Download our one-page brochure</a> for a summary of why you should choose Drupal, and information on its features and benefits.</p>
+      
+      <h3>Getting Started</h3>
+      <p>The Drupal Core is a collection of files that you must download in order to get started with Drupal. </p>
+      <a href="#" class="link-button"><span>Download Drupal 6.7</span></a>
+
+      <p class="clear">You can then build upon that with <a href="#">modules</a>, <a href="#">themes</a>, and <a href="#">translations.</a></p>
+    </div>
+  </div>
+      
+  <div class="grid-4 omega">
+    <h3>Technical Requirements</h3>
+    <div class="aboutsmall">
+      <p>For Drupal 6, you need the following to get started:</p>
+      <ul>
+        <li><strong>Web Server</strong>: <a href="#">Apache</a> or <a href="#">Microsoft IIS</a></li>
+        <li><strong>PHP</strong>: <a href="#"><a href="#">5.2</a> or higher recommended; PHP 4.3.5 required</li>
+        <li><strong>Database Server</strong>: <a href="#">MySQL</a> - 4.1 or higher or <a href="#">PostgreSQL</a> - 7.4 or higher</li>
+      </ul>
+      <p>Drupal is web-based, which means it is compatible with all <a href="#">operating systems</a>.</p>
+      <p>More detailed information on the <a href="#">system requirements</a>.</p>
+    </div>
+  </div>
+</div>
