fcbabfe93d0c3ba09e10c8191651f75ec47efe10
Gábor Hojtsy <gabor@hojtsy.hu>
2009-02-12 17:05:50 +0000

    #373457 by mikejoconnor: updates to the documentation landing page

--- drupalorg/drupalorg-documentation-index.tpl.php
+++ drupalorg/drupalorg-documentation-index.tpl.php
@@ -1,4 +1,38 @@
-<p style="color: red;"><blink><em>This page needs to be themed.  This is outputted by <code>drupalorg-download-index.tpl.php</code>.  All the dynamic elements are provided as a variable.  *blink* *blink*</em></blink></p>
-
-<p>Update: there are no dynamic elements for now -- we need feedback from Mark Boulton first.</p>
-
+<h2>Categories</h2>
+<p>If you have a document that would fit into one of these categories then feel free to add it for other Drupal users to read. Perhaps you have written a translation guide or some advice for new users. Alternatively, you might want to <a href="#">start a category</a> of your own.</p>
+<div class="grid-4 alpha">
+  <h5>About Drupal</h5>
+  <ul class="flat">
+    <li><a href="http://drupal.org/getting-started/6">Using Drupal</a></li>
+    <li><a href="http://drupal.org/books">Books About Drupal</a></li>
+    <li><a href="http://drupal.org/cases">Drupal Case Studies</a></li>
+  </ul>
+  <h5>Beyond the Basics</h5>
+  <ul class="flat">
+    <li><a href="http://drupal.org/handbook/customization/tutorials">Tutorials</a></li>
+    <li><a href="http://drupal.org/handbook/customization/snippets">Drupal Recipes</a></li>
+    <li><a href="http://drupal.org/node/21951">Understanding Drupal</a></li>
+  </ul>
+</div>
+<div class="grid-4">
+  <h5>Drupal in Your Language</h5>
+  <ul class="flat">
+    <li><a href="http://drupalfr.org/">French</a></li>
+    <li><a href="http://www.drupalitalia.org/">Italian</a></li>
+    <li><a href="http://www.drupal.ru/">Russian</a></li>
+  </ul>
+  <h5>Developing for Drupal</h5>
+  <ul class="flat">
+    <li><a href="http://drupal.org/node/508">Module Developers' Guide</a></li>
+    <li><a href="http://drupal.org/update/modules">Updating Your Modules</a></li>
+    <li><a href="http://api.drupal.org/api/">Drupal's APIs</a></li>
+  </ul>
+</div>
+<div class="grid-4 omega">
+  <h5>Get Involved</h5>
+  <ul class="flat">
+    <li><a href="http://drupal.org/node/502">Give Feedback</a></li>
+    <li><a href="http://drupal.org/node/314178">Talk With the Community</a></li>
+    <li><a href="http://drupal.org/node/317">Drupal's Work Space: the Issue Queue</a></li>
+  </ul>
+</div>
