98e3cb561c66e4f68d46986cb074760a4df6d37f
Gábor Hojtsy <gabor@hojtsy.hu>
2009-02-13 15:33:25 +0000

    #373958 my mikejoconnor: community page template

---
+++ drupalorg/drupalorg-community.tpl.php
@@ -0,0 +1,68 @@
+
+<div class="grid-8 alpha">
+  <h3>Recent activity</h3>
+  <form>
+    <p>Browse
+    <select value="Category">
+      <option selected="selected">Group</option>
+    </select> 
+    &nbsp;&nbsp; by 
+    <select value="date">
+      <option selected="selected">Date</option>
+    </select></p>
+  </form>
+
+  <h5><a href="#">Direct hook access to the query</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 13, 2008</p>
+  <h5><a href="#">add 'commit' as a valid GET arg</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 13, 2008</p>
+  <h5><a href="#">Guidelines for using Drupal's CVS contrib repository</a> <a href="#" class="tag">Add to dashboard</a></h5>
+  <p class="event">Last activity: September 14, 2008</p>
+  <h5><a href="#">Is this module being maintained?</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 15, 2008</p>
+  <h5><a href="#">CVS commit error</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 15, 2008</p>
+  <h5><a href="#">Direct hook access to the query</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 15, 2008</p>
+  <h5><a href="#">add 'commit' as a valid GET arg</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 15, 2008</p>
+  <h5><a href="#">Guidelines for using Drupal's CVS contrib repository</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 16, 2008</p>
+  <h5><a href="#">Is this module being maintained?</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 17, 2008</p>
+  <h5><a href="#">CVS commit error</a> <a href="#"><span class="icons icon_add">&nbsp;</span></a></h5>
+  <p class="event">Last activity: September 17, 2008</p>
+  <br>
+  
+</div>
+<div class="grid-4 omega">
+  <div class="community_block_heading grid-3 alpha">
+    <h3>Where is the<br /> Drupal Community?</h3>
+  </div>
+  <div class="grid-1 omega">
+    <img src="<?php print base_path() . drupal_get_path('module', 'drupalorg') ;?>/images/mascotbig.jpg" alt="Drupal Community Mascot" />
+  </div>
+  <div class="grid-4 alpha omega">
+    <h5>Commercial Support</h5>
+    <p>If you need help with a particular service then check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="#">hosting</a>, <a href="#">training</a>, <a href="#">development</a>, <a href="#">support services</a>, <a href="#">content moderation</a>, <a href="#">spam blocking</a>, and <a href="#">more.</a></p>
+    <h5>Local Groups</h5>
+    <p>With a <a href="#">worldwide community</a>, chances are there is a <a href="#">Drupal group</a> close to where you are. If you can't find a local group, why not set one up? It's a great chance for meeting community members face to face.</p>
+    <h5>Online Groups</h5>
+    <p><a href="#">Groups.drupal.org</a> provides a place for groups to organize, plan, and work on projects. Anyone can start a group and it's a great way to get involved and gain some support</p>
+    <h5>Chat (IRC)</h5>
+    <p><a href="#">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
+    <h5>Mailing Lists</h5>
+    <p>There are <a href="#">many mailing lists</a> you can subscribe to. You can also sign up to <a href="#">receive security announcements</a> and <a href="#">project issue updates.</a></p>
+    <h5>Forum</h5>
+    <p><a href="#">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="#">contribute</a> and <a href="#">help others.</a></p>
+    <h5>Events</h5>
+    <p>Community members can also be found at Drupal <a href="#">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
+    <h5>Shop</h5>
+    <p>Some of our community are even designing their own Drupal merchandise, which you can view and buy in our <a href="#">shop</a>.</p>
+  </div>
+  <?php if ($advertisement) : ?>
+    <div class="grid-4 alpha omega advertisement">
+      <p><a href="#">Advertising</a> funds the all-volunteer Drupal Association members to deal with serious issues such as hardware upgrades.</p>
+    </div>
+  <?php endif;?>
+</div>
