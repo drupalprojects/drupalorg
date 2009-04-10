0ff32404c7964a939451c34896be1b0145fe8780
Neil Drumm <drumm@3064.no-reply.drupal.org>
2009-04-10 18:10:08 +0000

    Moving to the dashboard project

--- dashboard/templates/dashboard-gadget.tpl.php
+++
@@ -1,6 +0,0 @@
-<div id="gadget-<?php print $gadget['gadget_id'] ?>" class="gadget grid-4 alpha omega">
-  <h2><?php print $gadget['title']; ?></h2>
-  <div class="content">
-    <?php print $gadget['content']; ?>
-  </div>
-</div>
