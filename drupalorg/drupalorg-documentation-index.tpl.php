66c62cf5a930fb897eee4d130557c055691a366a
Courtney Miller <courtney@5253.no-reply.drupal.org>
2009-04-11 22:53:32 +0000

    changed drupal.org links to be relative

--- drupalorg/drupalorg-documentation-index.tpl.php
+++ drupalorg/drupalorg-documentation-index.tpl.php
@@ -3,15 +3,15 @@
 <div class="grid-4 alpha">
   <h5>About Drupal</h5>
   <ul class="flat">
-    <li><a href="http://drupal.org/getting-started/6">Using Drupal</a></li>
-    <li><a href="http://drupal.org/books">Books About Drupal</a></li>
-    <li><a href="http://drupal.org/cases">Drupal Case Studies</a></li>
+    <li><a href="/getting-started/6">Using Drupal</a></li>
+    <li><a href="/books">Books About Drupal</a></li>
+    <li><a href="/cases">Drupal Case Studies</a></li>
   </ul>
   <h5>Beyond the Basics</h5>
   <ul class="flat">
-    <li><a href="http://drupal.org/handbook/customization/tutorials">Tutorials</a></li>
-    <li><a href="http://drupal.org/handbook/customization/snippets">Drupal Recipes</a></li>
-    <li><a href="http://drupal.org/node/21951">Understanding Drupal</a></li>
+    <li><a href="/handbook/customization/tutorials">Tutorials</a></li>
+    <li><a href="/handbook/customization/snippets">Drupal Recipes</a></li>
+    <li><a href="/node/21951">Understanding Drupal</a></li>
   </ul>
 </div>
 <div class="grid-4">
@@ -23,16 +23,16 @@
   </ul>
   <h5>Developing for Drupal</h5>
   <ul class="flat">
-    <li><a href="http://drupal.org/node/508">Module Developers' Guide</a></li>
-    <li><a href="http://drupal.org/update/modules">Updating Your Modules</a></li>
+    <li><a href="/node/508">Module Developers' Guide</a></li>
+    <li><a href="/update/modules">Updating Your Modules</a></li>
     <li><a href="http://api.drupal.org/api/">Drupal's APIs</a></li>
   </ul>
 </div>
 <div class="grid-4 omega">
   <h5>Get Involved</h5>
   <ul class="flat">
-    <li><a href="http://drupal.org/node/502">Give Feedback</a></li>
-    <li><a href="http://drupal.org/node/314178">Talk With the Community</a></li>
-    <li><a href="http://drupal.org/node/317">Drupal's Work Space: the Issue Queue</a></li>
+    <li><a href="/node/502">Give Feedback</a></li>
+    <li><a href="/node/314178">Talk With the Community</a></li>
+    <li><a href="/node/317">Drupal's Work Space: the Issue Queue</a></li>
   </ul>
 </div>
