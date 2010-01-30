56e7fcff714e8b66dacbc81cb10f4fd1dcacdf93
Derek Wright <git@dwwright.net>
2010-01-30 09:15:11 +0000

    #699696 by dww: Exported drupalorg_admin_comments_by_user to a separate file.

---
+++ views/drupalorg_admin_comments_by_user.view.php
@@ -0,0 +1,241 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Administrative view for cleaning up spam comments per user.
+ */
+
+$view = new view;
+$view->name = 'drupalorg_admin_comments_by_user';
+$view->description = 'Administrative view of all comments by a user';
+$view->tag = '';
+$view->view_php = '';
+$view->base_table = 'comments';
+$view->is_cacheable = FALSE;
+$view->api_version = 2;
+$view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
+$handler = $view->new_display('default', 'Defaults', 'default');
+$handler->override_option('relationships', array(
+  'nid' => array(
+    'label' => 'Node',
+    'required' => 0,
+    'id' => 'nid',
+    'table' => 'comments',
+    'field' => 'nid',
+    'relationship' => 'none',
+  ),
+  'uid' => array(
+    'label' => 'User',
+    'required' => 0,
+    'id' => 'uid',
+    'table' => 'comments',
+    'field' => 'uid',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('fields', array(
+  'title' => array(
+    'label' => 'Node title',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'strip_tags' => 0,
+      'html' => 0,
+    ),
+    'link_to_node' => 1,
+    'exclude' => 0,
+    'id' => 'title',
+    'table' => 'node',
+    'field' => 'title',
+    'relationship' => 'nid',
+  ),
+  'subject' => array(
+    'id' => 'subject',
+    'table' => 'comments',
+    'field' => 'subject',
+  ),
+  'timestamp' => array(
+    'id' => 'timestamp',
+    'table' => 'comments',
+    'field' => 'timestamp',
+  ),
+  'delete_comment' => array(
+    'label' => 'Operations',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'strip_tags' => 0,
+      'html' => 0,
+    ),
+    'text' => '',
+    'exclude' => 0,
+    'id' => 'delete_comment',
+    'table' => 'comments',
+    'field' => 'delete_comment',
+    'relationship' => 'none',
+  ),
+  'edit_comment' => array(
+    'id' => 'edit_comment',
+    'table' => 'comments',
+    'field' => 'edit_comment',
+  ),
+));
+$handler->override_option('arguments', array(
+  'uid' => array(
+    'default_action' => 'not found',
+    'style_plugin' => 'default_summary',
+    'style_options' => array(),
+    'wildcard' => 'all',
+    'wildcard_substitution' => 'All',
+    'title' => 'Comments by %1',
+    'breadcrumb' => '',
+    'default_argument_type' => 'fixed',
+    'default_argument' => '',
+    'validate_type' => 'user',
+    'validate_fail' => 'not found',
+    'break_phrase' => 0,
+    'not' => 0,
+    'id' => 'uid',
+    'table' => 'users',
+    'field' => 'uid',
+    'relationship' => 'uid',
+    'validate_user_argument_type' => 'either',
+    'validate_user_roles' => array(
+      '2' => 0,
+      '3' => 0,
+      '6' => 0,
+      '8' => 0,
+      '5' => 0,
+      '12' => 0,
+      '4' => 0,
+      '7' => 0,
+    ),
+    'default_options_div_prefix' => '',
+    'default_argument_user' => 0,
+    'default_argument_fixed' => '',
+    'validate_argument_node_type' => array(
+      'image' => 0,
+      'forum' => 0,
+      'project_project' => 0,
+      'project_release' => 0,
+      'project_issue' => 0,
+      'book' => 0,
+      'page' => 0,
+      'story' => 0,
+    ),
+    'validate_argument_node_access' => 0,
+    'validate_argument_nid_type' => 'nid',
+    'validate_argument_vocabulary' => array(
+      '1' => 0,
+      '40' => 0,
+      '42' => 0,
+      '38' => 0,
+      '6' => 0,
+      '9' => 0,
+      '4' => 0,
+      '3' => 0,
+      '7' => 0,
+      '2' => 0,
+      '5' => 0,
+      '31' => 0,
+      '32' => 0,
+      '33' => 0,
+      '34' => 0,
+    ),
+    'validate_argument_type' => 'tid',
+    'validate_argument_transform' => 0,
+    'validate_user_restrict_roles' => 0,
+    'validate_argument_project_term_vocabulary' => array(
+      '3' => 0,
+    ),
+    'validate_argument_project_term_argument_type' => 'tid',
+    'validate_argument_project_term_argument_action_top_without' => 'pass',
+    'validate_argument_project_term_argument_action_top_with' => 'pass',
+    'validate_argument_project_term_argument_action_child' => 'pass',
+  ),
+));
+$handler->override_option('access', array(
+  'type' => 'role',
+  'role' => array(
+    '3' => 3,
+    '4' => 4,
+  ),
+));
+$handler->override_option('cache', array(
+  'type' => 'none',
+));
+$handler->override_option('items_per_page', 25);
+$handler->override_option('use_pager', '1');
+$handler->override_option('style_plugin', 'table');
+$handler->override_option('style_options', array(
+  'grouping' => '',
+  'override' => 1,
+  'sticky' => 0,
+  'order' => 'desc',
+  'columns' => array(
+    'title' => 'title',
+    'subject' => 'subject',
+    'timestamp' => 'timestamp',
+    'delete_comment' => 'delete_comment',
+    'edit_comment' => 'delete_comment',
+  ),
+  'info' => array(
+    'title' => array(
+      'sortable' => 1,
+      'separator' => '',
+    ),
+    'subject' => array(
+      'sortable' => 1,
+      'separator' => '',
+    ),
+    'timestamp' => array(
+      'sortable' => 1,
+      'separator' => '',
+    ),
+    'delete_comment' => array(
+      'separator' => ' | ',
+    ),
+    'edit_comment' => array(
+      'separator' => '',
+    ),
+  ),
+  'default' => 'timestamp',
+));
+$handler = $view->new_display('page', 'Page', 'page_1');
+$handler->override_option('path', 'user/%/admin-comments');
+$handler->override_option('menu', array(
+  'type' => 'tab',
+  'title' => 'Administer comments',
+  'description' => '',
+  'weight' => '10',
+  'name' => 'navigation',
+));
+$handler->override_option('tab_options', array(
+  'type' => 'none',
+  'title' => '',
+  'description' => '',
+  'weight' => 0,
+));
