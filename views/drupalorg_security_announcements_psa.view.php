2416cd7e50236436e362a53df37e1ea824fd2d95
Derek Wright <git@dwwright.net>
2010-01-30 09:13:01 +0000

    #699696 by dww: Split default views into separate files and synced the views from DRUPAL-6--1 and HEAD.

---
+++ views/drupalorg_security_announcements_psa.view.php
@@ -0,0 +1,128 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Security Public Service Announcements.
+ */
+
+$view = new view;
+$view->name = 'drupalorg_security_announcements_psa';
+$view->description = 'Security PSAs';
+$view->tag = 'security';
+$view->view_php = '';
+$view->base_table = 'node';
+$view->is_cacheable = FALSE;
+$view->api_version = 2;
+$view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
+$handler = $view->new_display('default', 'Defaults', 'default');
+$handler->override_option('sorts', array(
+  'created' => array(
+    'order' => 'DESC',
+    'granularity' => 'second',
+    'id' => 'created',
+    'table' => 'node',
+    'field' => 'created',
+    'override' => array(
+      'button' => 'Override',
+    ),
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('filters', array(
+  'status_extra' => array(
+    'operator' => '=',
+    'value' => '',
+    'group' => '0',
+    'exposed' => FALSE,
+    'expose' => array(
+      'operator' => FALSE,
+      'label' => '',
+    ),
+    'id' => 'status_extra',
+    'table' => 'node',
+    'field' => 'status_extra',
+    'relationship' => 'none',
+  ),
+  'tid' => array(
+    'operator' => 'and',
+    'value' => array(
+      '1856' => '1856',
+    ),
+    'group' => '0',
+    'exposed' => FALSE,
+    'expose' => array(
+      'operator' => FALSE,
+      'label' => '',
+    ),
+    'type' => 'select',
+    'vid' => '1',
+    'id' => 'tid',
+    'table' => 'term_node',
+    'field' => 'tid',
+    'hierarchy' => 0,
+    'override' => array(
+      'button' => 'Override',
+    ),
+    'relationship' => 'none',
+    'reduce_duplicates' => 0,
+  ),
+));
+$handler->override_option('access', array(
+  'type' => 'none',
+));
+$handler->override_option('title', 'Security public service announcements');
+$handler->override_option('header', 'Security-related announcements, such as information on best practices. These posts by the Drupal security team are also sent to the security announcements e-mail list.');
+$handler->override_option('header_format', '1');
+$handler->override_option('header_empty', 1);
+$handler->override_option('use_pager', '1');
+$handler->override_option('row_plugin', 'node');
+$handler->override_option('row_options', array(
+  'teaser' => 1,
+  'links' => 1,
+  'comments' => 0,
+));
+$handler = $view->new_display('page', 'Page', 'page_1');
+$handler->override_option('path', 'security/psa');
+$handler->override_option('menu', array(
+  'type' => 'tab',
+  'title' => 'Public service anouncements',
+  'description' => '',
+  'weight' => '2',
+  'name' => 'navigation',
+));
+$handler->override_option('tab_options', array(
+  'type' => 'normal',
+  'title' => 'Security-related public service announcements',
+  'description' => 'These posts by the Drupal security team are also sent to the security announcements e-mail list.',
+  'weight' => '0',
+));
+$handler = $view->new_display('feed', 'Feed', 'feed_1');
+$handler->override_option('style_plugin', 'rss');
+$handler->override_option('style_options', array(
+  'mission_description' => FALSE,
+  'description' => '',
+));
+$handler->override_option('row_plugin', 'node_rss');
+$handler->override_option('row_options', array(
+  'item_length' => 'default',
+));
+$handler->override_option('path', 'security/psa/rss.xml');
+$handler->override_option('menu', array(
+  'type' => 'none',
+  'title' => '',
+  'description' => '',
+  'weight' => 0,
+  'name' => 'navigation',
+));
+$handler->override_option('tab_options', array(
+  'type' => 'none',
+  'title' => '',
+  'description' => '',
+  'weight' => 0,
+));
+$handler->override_option('displays', array(
+  'page_1' => 'page_1',
+  'default' => 0,
+));
+$handler->override_option('sitename_title', FALSE);
