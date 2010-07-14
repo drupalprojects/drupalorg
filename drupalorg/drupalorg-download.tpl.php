1a0be5bb0514b0e1a62eb6db90e06e80e93f2750
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-07-14 01:48:41 +0000

    Cleanup

--- drupalorg/drupalorg-download.tpl.php
+++ drupalorg/drupalorg-download.tpl.php
@@ -1,4 +1,4 @@
-<div id="download" class="grid-12 alpha omega">
+<div id="download" class="node-section grid-12 alpha omega">
 
   <div class="grid-3 alpha">
     <h2>Core</h2>
@@ -40,44 +40,42 @@
       <li><a href="#" class="all">All translations</a></li>
     </ul>
   </div>
+</div>
   
-  <hr>
+<div class="grid-12 alpha omega">
+  <h2>Drupal Modules</h2>
+  <p>Show only modules for Drupal version DROPDOWN</p>
+</div>
 
-  <div class="grid-12 alpha omega">
-    <h2>Drupal Modules</h2>
-    <p>Show only modules for Drupal version DROPDOWN</p>
-  </div>
-
-
-  <div class="grid-12 alpha omega">
+<div class="grid-12 alpha omega">
 
 <?php /*
   NOTE: It would be super helpful if these dynamic lists were wrapped in a <ul class="flat">
         This class removes the padding and bullets from the list items
 */ ?>
 
-    <div class="grid-3 alpha">
-        <h2>Most Installed</h2>
-        <?php print $most_popular_modules; ?>
-        <a href="#" class="all">More Most installed</a>
-    </div>
-    
-    <div class="grid-3">      
-      <h2>Most active modules</h2>
-      <?php print $most_active_modules; ?>
-      <a href="#" class="all">More most active</a>
-    </div>
-    
-    <div class="grid-3">      
-      <h2>New modules</h2>
-      <?php print $new_modules; ?>
-      <a href="#" class="all">More new modules</a>
-    </div>
+  <div class="grid-3 alpha">
+    <h2>Most Installed</h2>
+    <?php print $most_popular_modules; ?>
+    <a href="#" class="all">More Most installed</a>
+  </div>
   
-    <div class="grid-3 omega">        
-      <h2>Placeholder</h2>
-      <a href="#" class="all">More placeholder</a>
-    </div>
+  <div class="grid-3">      
+    <h2>Most active modules</h2>
+    <?php print $most_active_modules; ?>
+    <a href="#" class="all">More most active</a>
+  </div>
+  
+  <div class="grid-3">      
+    <h2>New modules</h2>
+    <?php print $new_modules; ?>
+    <a href="#" class="all">More new modules</a>
+  </div>
+  
+  <div class="grid-3 omega">        
+    <h2>Placeholder</h2>
+    <a href="#" class="all">More placeholder</a>
+  </div>
 
 </div>
 
@@ -105,7 +103,5 @@
     <h2>Placeholder</h2>
     <a href="#" class="all">More placeholder</a>
   </div>
-
-</div>
 
 </div>
