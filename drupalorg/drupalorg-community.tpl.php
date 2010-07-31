6b04c0514895244980d9fbe208aaa695b1f91ada
Lisa Rex <lisa.m.rex@gmail.com>
2010-07-31 18:10:35 +0000

    fixed #866574 by susanmacphee,lisarex: Added links and updated text.

--- drupalorg/drupalorg-community.tpl.php
+++ drupalorg/drupalorg-community.tpl.php
@@ -44,25 +44,23 @@
   </div>
   <div class="grid-4 alpha omega">
     <h5>Commercial Support</h5>
-    <p>If you need help with a particular service then check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="#">hosting</a>, <a href="#">training</a>, <a href="#">development</a>, <a href="#">support services</a>, <a href="#">content moderation</a>, <a href="#">spam blocking</a>, and <a href="#">more.</a></p>
+    <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking</a>.</p>
     <h5>Local Groups</h5>
-    <p>With a <a href="#">worldwide community</a>, chances are there is a <a href="#">Drupal group</a> close to where you are. If you can't find a local group, why not set one up? It's a great chance for meeting community members face to face.</p>
+    <p>With a worldwide community, chances are there is a <a href="http://groups.drupal.org/groups">Drupal group</a> close to where you are. If you can't find a local group, why not set one up? It's a great chance for meeting community members face to face.</p>
     <h5>Online Groups</h5>
-    <p><a href="#">Groups.drupal.org</a> provides a place for groups to organize, plan, and work on projects. Anyone can start a group and it's a great way to get involved and gain some support</p>
+    <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, plan, and work on projects. Anyone can start a group and it's a great way to get involved and gain some support.</p>
     <h5>Chat (IRC)</h5>
-    <p><a href="#">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
+    <p><a href="/irc">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
     <h5>Mailing Lists</h5>
-    <p>There are <a href="#">many mailing lists</a> you can subscribe to. You can also sign up to <a href="#">receive security announcements</a> and <a href="#">project issue updates.</a></p>
+    <p>There are <a href="/mailing-lists">many mailing lists</a> you can subscribe to. You can also sign up to receive <a href="/security">security announcements</a> and project issue updates from the project page.</p>
     <h5>Forum</h5>
-    <p><a href="#">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="#">contribute</a> and <a href="#">help others.</a></p>
+    <p><a href="/forum">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="/contribute">contribute</a> and <a href="/contribute/support">help others.</a></p>
     <h5>Events</h5>
-    <p>Community members can also be found at Drupal <a href="#">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
-    <h5>Shop</h5>
-    <p>Some of our community are even designing their own Drupal merchandise, which you can view and buy in our <a href="#">shop</a>.</p>
+    <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
   </div>
   <?php if ($advertisement) : ?>
     <div class="grid-4 alpha omega advertisement">
-      <p><a href="#">Advertising</a> funds the all-volunteer Drupal Association members to deal with serious issues such as hardware upgrades.</p>
+      <p><a href="/node/218094">Advertising</a> funds the all-volunteer Drupal Association members to deal with serious issues such as hardware upgrades.</p>
     </div>
   <?php endif;?>
 </div>
