21465716309f5a91f4340515b1931dcdf53cb977
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-08-19 00:26:09 +0000

    These should be sprites

--- drupalorg/drupalorg-home.tpl.php
+++ drupalorg/drupalorg-home.tpl.php
@@ -75,9 +75,9 @@
       <div id="rotate">
         <ul class="ui-tabs-nav">
           <li class="ui-tabs-selected"><a href="#fragment-1">News</a></li>
-          <li><a href="#fragment-2"><img src="<?php print $theme_images_directory; ?>/bullet_home_green.jpg"> Docs Updates</a></li>
-          <li><a href="#fragment-3"><img src="<?php print $theme_images_directory; ?>/bullet_home_purple.jpg"> Forum Posts</a></li>
-          <li><a href="#fragment-4"><img src="<?php print $theme_images_directory; ?>/bullet_home_red.jpg"> Commits</a></li>
+          <li><a href="#fragment-2">Docs Updates</a></li>
+          <li><a href="#fragment-3">Forum Posts</a></li>
+          <li><a href="#fragment-4">Commits</a></li>
         </ul>
         <div id="fragment-1" class="ui-tabs-panel">
           <?php print $tab_content_news; ?>
