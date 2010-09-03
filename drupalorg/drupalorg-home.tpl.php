3b5e80c7f648dfbafa83bde03294adb18269bbc5
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-09-03 06:36:17 +0000

    #852436 with help from attiks. Make various home page stats live.

--- drupalorg/drupalorg-home.tpl.php
+++ drupalorg/drupalorg-home.tpl.php
@@ -28,10 +28,10 @@
       <h2><a href="download">Develop with Drupal</a></h2>
       <p>Drupal is extensible, powerful, scalable, and flexible.</p>
       <div class="grid-2 alpha">
-       <h4><a href="cvs">Current activity</a></h4>
+       <h4>Current activity</h4>
        <ul>
-         <li><a href="#">4212 CVS a/c holders</a></li>
-         <li><a href="#">612 commits this month</a></li>
+         <li><a href="cvs"><?php print $number_of_cvs_accounts; ?> CVS a/c holders</a></li>
+         <li><a href="cvs"><?php print $number_of_cvs_commits; ?> commits this month</a></li>
        </ul>
       </div>
       <div class="grid-2 omega">
