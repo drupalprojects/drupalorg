2076824a82cede6f83c15f2d221fb0840a716b27
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-09-03 21:52:07 +0000

    JSLint

--- drupalorg/js/home.js
+++ drupalorg/js/home.js
@@ -1,11 +1,10 @@
-$(function() {
-  jQuery.bt.defaults.closeWhenOthersOpen = true;
-  var items = Drupal.settings.homePageMap, current = 0, max = Drupal.settings.homePageMap.length;
-  for (i = 0; i < items.length; i++) {
-    item = items[i];
-    css = { top: item.top, left: item.left };
-    $('.homepage-map').append($('<img class="homepage-image" src="'+ Drupal.settings.basePath +'/sites/all/themes/trunk/images/homepage-'+ item.type +'.png" width="10" height="10" id="homepage-image-'+ i +'" />').css(css));
-    $('#homepage-image-'+ i).bt(item.content, {
+$(function () {
+  var items = Drupal.settings.homePageMap, current = 0, max = Drupal.settings.homePageMap.length, i;
+  $.bt.defaults.closeWhenOthersOpen = true;
+  for (i = 0; i < items.length; i += 1) {
+    var item = items[i];
+    $('.homepage-map').append($('<img class="homepage-image" src="' + Drupal.settings.basePath + '/sites/all/themes/trunk/images/homepage-' + item.type + '.png" width="10" height="10" id="homepage-image-' + i + '" />').css({ top: item.top, left: item.left }));
+    $('#homepage-image-' + i).bt(item.content, {
       width: 220,
       spikeLength: 15,
       spikeGirth: 10,
@@ -19,8 +18,8 @@ $(function() {
       overlap: 5
     });
   }
-  var update = function() {
-    var $element = $('#homepage-image-'+ current),
+  var update = function () {
+    var $element = $('#homepage-image-' + current),
         $window = $(window),
         top = $window.scrollTop(),
         bottom = $window.height() + top,
@@ -29,36 +28,35 @@ $(function() {
     if (top < elementBottom && bottom > elementBottom) {
       $element.btOn();
     }
-    current++;
-    if (current == max) {
+    current += 1;
+    if (current === max) {
       current = 0;
     }
-    setTimeout(function() {
+    setTimeout(function () {
       update();
     }, 3000);
   };
   update();
 });
 
-Drupal.behaviors.things_we_made = function(context) {
+Drupal.behaviors.things_we_made = function (context) {
   var left = 0;
-  var items = $('.things-we-made li');
   var index = 0;
 
-  setInterval(function() {
+  setInterval(function () {
     // Figure which image we are wrapping too
     if ($('.things-we-made li').eq(index).hasClass('last')) {
       index = 0;
     }
     else {
-      index++;
+      index += 1;
     }
     left = index * -300;
 
-    $('ul.things-we-made').animate({ marginLeft: left, }, 500);
+    $('ul.things-we-made').animate({ marginLeft: left }, 500);
   }, 6000);
-}
+};
 
-Drupal.behaviors.frontTabs = function() {
+Drupal.behaviors.frontTabs = function () {
   $('#rotate > ul').tabs({}).tabs('rotate', 0);
 };
