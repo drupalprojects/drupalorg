8109ecfe5968b44cc549400c78a0a7fe7c67f163
Lisa Rex <lisa.m.rex@gmail.com>
2010-09-16 20:36:22 +0000

    #909570 by Alex UA merged Online Groups and Local Groups on Community page, #912946 by kaakuu reorganized 'where is the drupal community' links

--- drupalorg/drupalorg-community.tpl.php
+++ drupalorg/drupalorg-community.tpl.php
@@ -43,20 +43,18 @@
     <img src="<?php print base_path() . drupal_get_path('module', 'drupalorg') ;?>/images/mascotbig.jpg" alt="Drupal Community Mascot" />
   </div>
   <div class="grid-4 alpha omega">
+    <h5>Online and Local Groups</h5>
+    <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, meet, and work on projects based on interest or geographic location. It's a great way to get involved, learn more and get support.</p>
+    <h5>Events</h5>
+    <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
     <h5>Commercial Support</h5>
     <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking</a>.</p>
-    <h5>Local Groups</h5>
-    <p>With a worldwide community, chances are there is a <a href="http://groups.drupal.org/groups">Drupal group</a> close to you. If you can't find a local group, why not set one up? It's a great chance for meeting community members face to face.</p>
-    <h5>Online Groups</h5>
-    <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, plan, and work on projects. Anyone can start a group and it's a great way to get involved and gain some support.</p>
     <h5>Chat (IRC)</h5>
     <p><a href="/irc">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
     <h5>Mailing Lists</h5>
     <p>There are <a href="/mailing-lists">many mailing lists</a> you can subscribe to. You can also sign up to receive <a href="/security">security announcements</a> and project issue updates from the project page.</p>
     <h5>Forum</h5>
     <p><a href="/forum">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="/contribute">contribute</a> and <a href="/contribute/support">help others.</a></p>
-    <h5>Events</h5>
-    <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
   </div>
   <?php if ($advertisement) : ?>
     <div class="grid-4 alpha omega advertisement">
