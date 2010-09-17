dfa07b301c666ad81cf271566e434dd1401fc4b7
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-09-17 02:29:17 +0000

    #428410 Meta data block on documentation pages

--- drupalorg_handbook/handbook-meta-data.tpl.php
+++ drupalorg_handbook/handbook-meta-data.tpl.php
@@ -1,23 +1,11 @@
 <div class="meta">
   <h5>About This Article</h5>
   <ul>
-    <?php if ($versions): ?>
-      <li class="versions">Drupal versions: <?php print $versions; ?>
-    <?php endif; ?>
+    <?php foreach ($info as $title => $terms): ?>
+      <li><?php print $title; ?>: <?php print $terms; ?>
+    <?php endforeach; ?>
     <?php if ($modified): ?>
       <li class="modified">Last modified: <?php print $modified; ?></li>
     <?php endif; ?>
-    <?php if ($authors): ?>
-      <li class="authors">Authors: <?php print $authors; ?></li>
-    <?php endif; ?>
-    <?php if ($languages): ?>
-      <li class="languages">Languages: <?php print $languages; ?><a href="#">Cymraeg</a>, <a href="#">English</a> </li>
-    <?php endif; ?>
-    <?php if ($features): ?>
-      <li class="feature-tags">Tags: <?php print $features; ?></li>
-    <?php endif; ?>
-    <?php if ($alerts): ?>
-      <?php print $alerts; ?>
-    <?php endif;?>
   </ul>
 </div>
