140a7e42619cf3d59acc2152a474246533d077f3
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-09-24 00:51:42 +0000

    #921052 fix druplicon

--- drupalorg/drupalorg-community.tpl.php
+++ drupalorg/drupalorg-community.tpl.php
@@ -6,27 +6,19 @@
   
 </div>
 <div class="grid-4 omega">
-  <div class="community_block_heading grid-3 alpha">
-    <h3>Where is the<br /> Drupal Community?</h3>
-  </div>
-  <div class="grid-1 omega">
-    <img src="<?php print base_path() . drupal_get_path('module', 'drupalorg') ;?>/images/mascotbig.jpg" alt="Drupal Community Mascot" />
-  </div>
-  <div class="grid-4 alpha omega">
-    <h5>Online and Local Groups</h5>
-    <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, meet, and work on projects based on interest or geographic location. It's a great way to get involved, learn more and get support.</p>
-    <h5>Events</h5>
-    <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
-    <h5>Commercial Support</h5>
-    <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking</a>.</p>
-    <h5>Chat (IRC)</h5>
-    <p><a href="/irc">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
-    <h5>Forum</h5>
-    <p><a href="/forum">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="/contribute">contribute</a> and <a href="/contribute/support">help others.</a></p>
-    <h5>Mailing Lists</h5>
-    <p>There are <a href="/mailing-lists">many mailing lists</a> you can subscribe to. You can also sign up to receive <a href="/security">security announcements</a> and project issue updates from the project page.</p>
-
-  </div>
+  <h3 class="community-title">Where is the<br /> Drupal Community?</h3>
+  <h5>Online and Local Groups</h5>
+  <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, meet, and work on projects based on interest or geographic location. It's a great way to get involved, learn more and get support.</p>
+  <h5>Events</h5>
+  <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
+  <h5>Commercial Support</h5>
+  <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking</a>.</p>
+  <h5>Chat (IRC)</h5>
+  <p><a href="/irc">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
+  <h5>Forum</h5>
+  <p><a href="/forum">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="/contribute">contribute</a> and <a href="/contribute/support">help others.</a></p>
+  <h5>Mailing Lists</h5>
+  <p>There are <a href="/mailing-lists">many mailing lists</a> you can subscribe to. You can also sign up to receive <a href="/security">security announcements</a> and project issue updates from the project page.</p>
   <?php if ($advertisement) : ?>
     <div class="grid-4 alpha omega advertisement">
       <p><a href="/node/218094">Advertising</a> funds the all-volunteer Drupal Association members to deal with serious issues such as hardware upgrades.</p>
