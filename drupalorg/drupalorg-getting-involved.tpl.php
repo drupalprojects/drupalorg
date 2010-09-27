71c42bc9f4975a379d00665f8bfabbf64489bdaa
jenlampton <jenlampton@85586.no-reply.drupal.org>
2010-09-27 06:16:56 +0000

    #660734 by jenlampton: create custom callback and page template for the getting involved page, includes db update to remove old url alias, and new view

---
+++ drupalorg/drupalorg-getting-involved.tpl.php
@@ -0,0 +1,10 @@
+<div class="node-section span-16 alpha omega" id="getting-involved">
+				
+	<div class="docs">
+	  <?php print $docs_node ?>
+	</div>
+
+	<h3><?php print $spotlight_title ?></h3>
+	<?php print $spotlight ?>
+
+</div>
