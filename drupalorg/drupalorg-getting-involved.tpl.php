afeb3630d1af7cfc1a0e2f3599a1493db8f7ed05
jenlampton <jenlampton@85586.no-reply.drupal.org>
2010-09-30 22:03:11 +0000

    #926298 by jenlampton: rework getting-involved page

--- drupalorg/drupalorg-getting-involved.tpl.php
+++ drupalorg/drupalorg-getting-involved.tpl.php
@@ -1,10 +1,18 @@
-<div class="node-section span-16 alpha omega" id="getting-involved">
+<div id="getting-involved">
 				
-	<div class="docs">
-	  <?php print $docs_node ?>
+	<div class="grid-4 alpha">
+	  <div id="node-281873" class="node node-type-book clear-block">
+	    <div class="node-content">
+        <?php print $docs_node ?>
+        <?php print $docs_more ?>
+      </div>
+    </div>
 	</div>
 
-	<h3><?php print $spotlight_title ?></h3>
-	<?php print $spotlight ?>
-
+  <div class="grid-4 omega">
+	  <h2><?php print $spotlight_title ?></h2>
+	  <?php print $spotlight ?>
+	</div>
+	
+  <div class="clear-block"></div>
 </div>
