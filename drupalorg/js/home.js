f433c6989ffb70f9787b0c5208cfd1fba790f8bd
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-10-11 05:13:57 +0000

    replacing Beautytips with CSS. Was too much IE compatibility trouble for 30k.

--- drupalorg/js/home.js
+++ drupalorg/js/home.js
@@ -2,40 +2,20 @@ $(function () {
   var current = 0,
     $map = $('.homepage-map'),
     $pins = $('.homepage-map>.homepage-pin');
+
   $pins.each(function () {
     $(this).css({
       left: '' + Drupal.html5UserGeolocationLongitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
       bottom: '' + Drupal.html5UserGeolocationLatitudeToPx($('>.latitude', this).text(), 78, -58, $map.height()) + 'px'
     });
-  }).bt({
-    contentSelector: "$('>.content', this)",
-    positions: ['left', 'top'],
-    windowMargin: -1000,
-    closeWhenOthersOpen: true,
-    clickAnywhereToClose: false,
-    width: 'auto',
-    spikeLength: 15,
-    spikeGirth: 10,
-    padding: 5,
-    cornerRadius: 10,
-    fill: '#FFF',
-    strokeStyle: '#BFBFBF',
-    strokeWidth: 1,
-    trigger: 'none',
-    preShow: function () {
-      $(this).show();
-    },
-    postHide: function () {
-      $(this).hide();
-    },
-    overlap: 4
-  });
+  }).eq(current).show();
   setInterval(function () {
-    $pins.eq(current).btOn();
+    $pins.eq(current).hide();
     current += 1;
     if (current === $pins.length) {
       current = 0;
     }
+    $pins.eq(current).show();
   }, 4000);
 });
 
