aec93be955822df1af8050eb6b792319c376a4b6
Derek Wright <git@dwwright.net>
2010-10-18 21:41:24 +0000

    #943156 by c4rl: Re-added Druplicon to the /community page and other fixes.

--- drupalorg/drupalorg-community.tpl.php
+++ drupalorg/drupalorg-community.tpl.php
@@ -1,9 +1,9 @@
-
 <div class="grid-8 alpha">
+  <span class="druplicon-logo"></span>
   <h2 class="community-title">Where is the Drupal Community?</h2>
-  <h5>Online & Local Groups</h5>
+  <h5>Online &amp; Local Groups</h5>
   <p><a href="http://groups.drupal.org">Groups.drupal.org</a> provides a place for groups to organize, meet, and work on projects based on interest or geographic location. It's a great way to get involved, learn more and get support.</p>
-  <h5>Events & Meetups</h5>
+  <h5>Events &amp; Meetups</h5>
   <p>Community members can also be found at Drupal <a href="http://groups.drupal.org/event">events</a> where you can meet face to face, swap tips, and get inspiration for your next Drupal project, making friends along the way.</p>
   <h5>Chat (IRC)</h5>
   <p><a href="/irc">IRC</a> is one means to communicate and interact with others. Whether you are asking questions or giving answers, IRC is a fast and effective way of getting involved with the community and getting the support you need.</p>
@@ -12,18 +12,14 @@
   <h5>Community Spotlight</h5>
   <p>The <a href="/community-spotlight">Community Spotlight</a> highlights Drupal community members and teams. <a href="/forum/community-spotlight/how-to-nominate">Nominate others</a> if you have found their contributions worthwhile.</p>
   <h5>Commercial Support</h5>
-  <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking</a>.</p>
+  <p>If you need professional help, check out our <a href="/marketplace">Marketplace</a> where you can find companies on hand to assist with <a href="/hosting">hosting</a>, <a href="/training-services">training</a> and other <a href="/drupal-services">Drupal services</a> such as development, support, content moderation and spam blocking.</p>
   <h5>Forum</h5>
   <p><a href="/forum">Our forums</a> offer a huge knowledge bank to support you. It is also a good way for you to <a href="/contribute">contribute</a> and <a href="/contribute/support">help others.</a></p>
   <h5>Mailing Lists</h5>
   <p>There are many <a href="/mailing-lists">mailing lists</a> you can subscribe to, in addition to <a href="/security">security announcements</a>, project issue updates and posts on groups.drupal.org.</p>
-
-  
 </div>
 <div class="grid-4 omega">
   <?php print $help_form; ?>
   <h3>Recent activity</h3>
   <?php print $recent_activity; ?>
-  
-</div>
 </div>
