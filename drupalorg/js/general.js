de5063bc5ce6a150375536f2d58eafddae1ca0a0
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-11-09 03:37:40 +0000

    Move drupal.org-wide JS to the theme.

--- drupalorg/js/general.js
+++ drupalorg/js/general.js
@@ -11,28 +11,6 @@ Drupal.behaviors.drupalorgSetHome = function () {
   });
 };
 
-// All Drupal.org specific behaviors.
-Drupal.behaviors.Drupalorg = function () {
-  var $element = $('body:not(.drupalorg-front) #edit-search-theme-form-1'),
-    value = Drupal.t('Search Drupal.org');
-
-  // Add focus/blur label behavior to search box.
-  $element.bind('focus', function () {
-    if ($element.val() === value) {
-      $element.val('').addClass('has-value');
-    }
-  });
-  $element.bind('blur', function () {
-    if ($element.val() === '' || $element.val() === value) {
-      $element.val(value).removeClass('has-value');
-    }
-    else {
-      $element.addClass('has-value');
-    }
-  });
-  $element.trigger('blur');
-};
-
 Drupal.behaviors.drupalorgSearch = function () {
   $('body.page-search #content-top-region form:not(.drupalorgSearch-processed)').addClass('drupalorgSearch-processed').each(function () {
     var $this = $(this);
