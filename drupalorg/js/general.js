6006373d044089a52a1db7b68e70838232587702
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-11-09 03:24:16 +0000

    Remove footer moving.

--- drupalorg/js/general.js
+++ drupalorg/js/general.js
@@ -14,9 +14,7 @@ Drupal.behaviors.drupalorgSetHome = function () {
 // All Drupal.org specific behaviors.
 Drupal.behaviors.Drupalorg = function () {
   var $element = $('body:not(.front) #edit-search-theme-form-1'),
-    value = Drupal.t('Search Drupal.org'),
-    $footer = $('#footer'),
-    $page = $('#page');
+    value = Drupal.t('Search Drupal.org');
 
   // Add focus/blur label behavior to search box.
   $element.bind('focus', function () {
@@ -33,24 +31,6 @@ Drupal.behaviors.Drupalorg = function () {
     }
   });
   $element.trigger('blur');
-
-  // Push footer to bottom of window if content is smaller than window.
-  if ($footer.length > 0 && $page.length > 0) {
-    var footer_height = $footer.innerHeight();
-    var footer_pos = $footer.position();
-    var window_height = $(window).height();
-    var page_height = $page.height();
-    var page_pos = $page.position();
-    var page_total = page_pos.top + page_height;
-    var content_height = footer_height + page_total;
-    if (content_height <= window_height) {
-      if (footer_pos.top < window_height) {
-        var footer_new_pos = window_height - content_height;
-        $footer.css('position', 'relative');
-        $footer.css('margin-top', footer_new_pos);
-      }
-    }
-  }
 };
 
 Drupal.behaviors.drupalorgSearch = function () {
