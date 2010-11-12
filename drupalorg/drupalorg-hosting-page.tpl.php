b041a51c8a8c82130faa112030688f66ecbc7fae
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-11-12 07:45:48 +0000

    #936308 Hosting Ads

---
+++ drupalorg/drupalorg-hosting-page.tpl.php
@@ -0,0 +1,34 @@
+<h2>Drupal hosts that support the <a href="http://association.drupal.org/">Drupal Association</a></h2>
+
+<div class="clear-block">
+  <div class="grid-4 alpha">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-SupportDA-Hosting-A'); ?>
+  </div>
+  <div class="grid-4 omega">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-SupportDA-Hosting-B'); ?>
+  </div>
+</div>
+
+
+<h2>Enterprise managed Drupal hosting</h2>
+
+<div class="clear-block">
+  <div class="grid-4 alpha">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-Managed-Hosting-A'); ?>
+  </div>
+  <div class="grid-4 omega">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-Managed-Hosting-B'); ?>
+  </div>
+</div>
+
+
+<h2>Enterprise & high traffic hosting</h2>
+
+<div class="clear-block">
+  <div class="grid-4 alpha">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-Shared-Hosting-A'); ?>
+  </div>
+  <div class="grid-4 omega">
+    <?php print theme('google_admanager_block', variable_get('google_admanager_account', ''), 'Hosting-Shared-Hosting-B'); ?>
+  </div>
+</div>
