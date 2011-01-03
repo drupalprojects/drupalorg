1ac40185e305248025070b52fec8bc23a9e6addb
Neil Drumm <drumm@3064.no-reply.drupal.org>
2011-01-03 08:31:35 +0000

    #1013204 Stub translation

---
+++ drupalorg/translations/drupalorg-d7-announcement.ru.php
@@ -0,0 +1,107 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - Легче и мощнее, чем когда-либо',
+  'Friendly and powerful:' =>
+  'Легкий и мощный:',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  '',
+  'Get started with Drupal 7' =>
+  '',
+  'Runs on Drupal 7: @site' =>
+  '',
+  'Easier to use' =>
+  '',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  '',
+  'More flexible' =>
+  '',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  '',
+  'More scalable' =>
+  '',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  '',
+  'Open source' =>
+  '',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  '',
+  'Take a tour' =>
+  '',
+  'Learn more' =>
+  '',
+  'Get started with updated documentation for Drupal 7:' =>
+  '',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '',
+  '<a href="@api-url">API Documentation</a>' =>
+  '',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  '',
+  'Features' =>
+  '',
+  'Flexible content' =>
+  '',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  '',
+  'Better theming' =>
+  '',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  '',
+  'Accessible' =>
+  '',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  '',
+  'Images and files' =>
+  '',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  '',
+  'Automated code testing' =>
+  '',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  '',
+  'Improved database support' =>
+  '',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  '',
+  'Better distribution support' =>
+  '',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  '',
+  'Extend' =>
+  '',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  '',
+  'See Drupal 7 in action' =>
+  '',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  '',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '',
+  'Requirements' =>
+  '',
+  'To install Drupal 7, you will need:' =>
+  '',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  '',
+);
