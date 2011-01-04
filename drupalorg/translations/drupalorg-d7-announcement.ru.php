bc97bb522faca6a4d1bf46ab94eeb48abf6f9f60
tvn <tvn@999848.no-reply.drupal.org>
2011-01-04 21:49:16 +0000

    updated russian translation

--- drupalorg/translations/drupalorg-d7-announcement.ru.php
+++ drupalorg/translations/drupalorg-d7-announcement.ru.php
@@ -7,101 +7,104 @@ $strings = array(
   'Drupal 7' =>
   'Drupal 7',
   'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
-  '',
+  'Мы с гордостью представляем наше лучшее на данный момент творение &ndash; Drupal 7, легкую и мощную платформу для разработки сайтов практически любой сложности: от блогов и микросайтов до сайтов сообществ с развитыми функциями социальных сетей и совместной работы.',
   'Get started with Drupal 7' =>
-  '',
+  'Начни работу с Drupal 7',
   'Runs on Drupal 7: @site' =>
-  '',
-  'Easier to use' =>
-  '',
+  'Работает на Drupal 7: @site',
+  'Easier to use' => 
+  'Простота использования',
   'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
-  '',
+  'Полностью переработанный административный интерфейс позволяет быстрее выполнять ежедневные рутинные задачи. Проделана большая работа по повышению удобства работы с сайтом для администраторов и редакторов.',
   'More flexible' =>
-  '',
+  'Более гибкий',
   'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
-  '',
+  'Создавайте свою собственную структуру содержимого и добавляйте собственные поля к содержимому, данным пользователей, комментариям и прочему. Расширяйте возможности своего сайта при помощи более 800 модулей, уже доступных для Drupal 7.',
   'More scalable' =>
-  '',
+  'Более масштабируемый',
   'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
-  '',
+  'Ваш сайт на Drupal 7 будет быстрым, отзывчивым и способным обрабатывать громадное количество запросов благодаря улучшенной оптимизации JavaScript и CSS, усовершенствованному кешированию и многому другому.',
   'Open source' =>
-  '',
+  'Open source',
   'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
-  '',
+  'Приходите к нам за программным обеспечением - и становитесь частью нашего сообщества. Тысячи сообразительных и плодотворных людей работают совместно, чтобы постоянно делать лучше Drupal, модули, темы и сборки.',
   'Take a tour' =>
-  '',
+  'Посмотреть презентацию',
   'Learn more' =>
-  '',
+  'Узнать больше',
   'Get started with updated documentation for Drupal 7:' =>
-  '',
+  'Начните с обновленной документации для Drupal 7:',
   '<a href="@install-url">Installing Drupal 7</a>' =>
-  '',
+  '<a href="@install-url">Установка Drupal 7</a>',
   '<a href="@upgrade-url">Upgrading from version 6</a>' =>
-  '',
+  '<a href="@upgrade-url">Обновление с версии 6</a>',
   '<a href="@api-url">API Documentation</a>' =>
-  '',
+  'Документация API',
   'More topics in the <a href="@documentation-url">online documentation</a>.' =>
-  '',
+  'Больше статей в <a href="@documentation-url">онлайн-документации</a>.',
   'Features' =>
-  '',
+  'Возможности',
   'Flexible content' =>
-  '',
+  'Легко изменяемое содержимое',
   'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
-  '',
+'Создавайте собственные поля данных, которые могут быть использованы в типах материалов, данных пользователей, комментариях, терминах таксономии и прочих сущностях. Храните данные этих полей в SQL, <a href="@nosql-url">NoSQL</a>, или в удаленных хранилищах данных.',
   'Better theming' =>
-  '',
+  'Улучшенная темизация',
   'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
-  '',
+  'Решайте, что конкретно хотите вывести и где, с новым Render API и несколькими фундаментальными хуками пост-обработки. Новый модуль RDF предоставляет семантическую веб-разметку.',
   'Accessible' =>
-  '',
+  'Повышенная доступность',
   'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
-  '',
+  'Административная часть стала более доступной. Множество улучшений интерфейса упрощают работу по созданию сайтов, доступных для абсолютно всех пользователей.',
   'Images and files' =>
-  '',
+  'Изображения и файлы',
   'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
-  '',
+  'Добавлена встроенная поддержка вставки изображения в содержимое. Возможность генерации различных изображений для миниатюр, пред-просмотра и других типов изображений. Публичный и приватный методы загрузки файлов могут быть использованы одновременно.',
   'Automated code testing' =>
-  '',
+  'Автоматизированное тестирование кода',
   'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
-  '',
+  'Новый конструктор автоматизированного тестирования с более чем 30 000 встроенных тестов позволяет <a href="@qa-url">непрерывное тестирование</a> всех патчей ядра Drupal и распространяемых сообществом модулей.',
   'Improved database support' =>
-  '',
+  'Улучшенная поддержка баз данных',
   'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
-  '',
+  'Новый механизм (слой) работы с базами данных обеспечивает встроенную поддержку для SQLite, MySQL/MariaDB, and PostgreSQL. Установка дополнительных модулей позволяет использовать <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a> и другие базы данных.',
   'Better distribution support' =>
-  '',
+  'Улучшенная поддержка сборок',
   'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
-  '',
+  'Используйте инсталляционные профили, чтобы распространять свою сборку Drupal. Новые API и экспортируемые конфигурации позволяют описать ещё больше настроек в коде профиля.',
   'Extend' =>
-  '',
+  'Расширяемость',
   'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
-  '',
+  'Благодаря огромным усилиям сообщества, свыше 800 модулей полностью готовы для Drupal 7 или находятся в процессе активной разработки, включая <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, и <a href="@wysiwyg-url">WYSIWYG</a>, и их число каждый день растет.',
   'See Drupal 7 in action' =>
-  '',
+  'Посмотреть Drupal 7 в действии',
   'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
-  '',
+  'Drupal 7 уже используется на сотнях сайтов. Вот некоторые из них:',
   '<a href="@examiner-url">Examiner.com</a>' =>
-  '',
+  '<a href="@examiner-url">Examiner.com</a>',
   '<a href="@gardens-url">Drupal Gardens</a>' =>
-  '',
+  '<a href="@gardens-url">Drupal Gardens</a>',
   '<a href="@subhub-url">SubHub</a>' =>
-  '',
+  '<a href="@subhub-url">SubHub</a>',
   '<a href="@cpm-url">Chicago Public Media</a>' =>
-  '',
+  '<a href="@cpm-url">Chicago Public Media</a>',
   '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
-  '',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
   'Requirements' =>
-  '',
+  'Требования',
   'To install Drupal 7, you will need:' =>
-  '',
+  'Для установки Drupal 7, вам потребуются:',
   '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
-  '',
+  '<strong>Веб сервер</strong>: Apache (рекомендуется), Nginx, Lighttpd, или Microsoft IIS',
   '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
-  '',
+  '<strong>База данных</strong>: MySQL 5.0.15 и выше, PostgreSQL 8.3 и выше, или SQLite 3.x',
   '<strong>PHP</strong>: 5.2.4 and higher' =>
-  '',
+  '<strong>PHP</strong>: 5.2.4 и выше',
   '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
-  '',
+  '<strong>Память</strong>: 32MB (Для сайта, использующего дополнительные модули, может потребоваться 64 Мб памяти или больше.)',
   'More <a href="@requirements-url">requirements information</a>.' =>
-  '',
+  'Более подробная <a href="@requirements-url">информация о требованиях</a>.',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  'Примите участие в <a href="@drupalcon-url">DrupalCon Чикаго</a>, чтобы узнать больше о Drupal 7',
 );
+
