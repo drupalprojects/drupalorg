e534b85241f211f603aaf3b49ed1fda54c34fcc7
tvn <tvn@999848.no-reply.drupal.org>
2011-01-05 18:40:16 +0000

    Korean translation

---
+++ drupalorg/translations/drupalorg-d7-announcement.ko.php
@@ -0,0 +1,109 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - 더욱 쉽고 강력해졌습니다',
+  'Friendly and powerful:' =>
+  '편리하고 강력함:',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  'Drupal 7을 선보입니다. Drupal 7은 블로그와 소규모 웹사이트에서 협업 커뮤니티에 이르기까지 거의 모든 종류의 웹사이트를 만들 수 있는 편리하고 강력한 콘텐츠 관리 플랫폼입니다.',
+  'Get started with Drupal 7' =>
+  'Drupal 7 시작하기',
+  'Runs on Drupal 7: @site' =>
+  'Drupal 7을 이용하는 웹사이트: @site',
+  'Easier to use' =>
+  '더욱 사용하기 쉬운',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  '완전히 새로워진 관리자 인터페이스로 일상적인 작업을 더 쉽게 찾고 처리할 수 있습니다. 특히 사이트 구성과 콘텐츠 에디터가 많이 개선되었습니다.',
+  'More flexible' =>
+  '더욱  유연한',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  '당신의 상황에 맞도록 콘텐츠 구조를 정의하고 콘텐츠, 사용자, 댓글 등에 필드를 추가할 수 있습니다. 이미 Drupal 7을 지원하는 800개 이상의 모듈을 이용하여 사이트를 확장시킬 수 있습니다.',
+  'More scalable' =>
+  '더 확장성 있는',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  'Drupal 7 웹사이트는 빠르고 신속하게 응답하며 많은 트래픽을 처리할 수 있을 것입니다. 이를 위해 자바스크립트와 CSS 최적화가 이뤄지고 캐싱과 여러 부분이 개선되었습니다.',
+  'Open source' =>
+  '오픈소스',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  'Drupal 7은 커뮤니티와 함께합니다. 수천명의 사람들이 Drupal과 모듈, 테마와 배포판을 계속 개선하기 위해 함께 작업하고 있습니다.',
+  'Take a tour' =>
+  '둘러보기',
+  'Learn more' =>
+  '자세히 알아보기',
+  'Get started with updated documentation for Drupal 7:' =>
+  'Drupal 7을 위한 다음 문서들을 둘러보십시오.',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '<a href="@install-url">Drupal 7 설치하기</a>',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '<a href="@upgrade-url">Drupal 6에서 업그레이드하기</a>',
+  '<a href="@api-url">API Documentation</a>' =>
+  '<a href="@api-url">API 문서</a>',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  '더 많은 내용이 <a href="@documentation-url">온라인 문서</a>로 제공됩니다.',
+  'Features' =>
+  '기능',
+  'Flexible content' =>
+  '유연한 콘텐츠',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  '커스텀 필드를 정의하여 콘텐츠 유형, 사용자, 댓글, 태그와 다른 요소들과 함께 이용하십시오. 데이터는 SQL, <a href="@nosql-url">NoSQL</a> 스토리지에 저장하거나 원격 스토리지를 이용할 수도 있습니다.',
+  'Better theming' =>
+  '더 나은 테마 기능',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  '화면에 무엇이 어디에 보일지 새로운 Render API와 강력한 hook를 이용하여 정확하게 조정하십시오. 새로운 RDF 모듈은 시맨택 웹 마크업을 지원합니다.',
+  'Accessible' =>
+  '접근성',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  '관리자 화면은 더 사용하기 쉬워졌습니다. 많은 프론트엔드 개선으로 접근성 높은 사이트를 만들기 더 쉬워졌습니다.',
+  'Images and files' =>
+  '이미지와 파일',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  '이제 콘텐츠에 이미지를 추가하는 기능이 내장되었습니다. 썸네일, 미리보기와 같은 여러 이미지 스타일을 생성할 수 있습니다. 또한 파일들을 제공하는 방법이 개선되었습니다.',
+  'Automated code testing' =>
+  '자동 코드 테스트',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  '3만개가 넘는 기본 테스트가 내장된 새로운 자동 테스트 프레임워크를 통해 모든 Drupal 코어 패치와 모듈들의 <a href="@qa-url">지속적 통합 테스트</a>를 수행할 수 있습니다.',
+  'Improved database support' =>
+  '개선된 데이터베이스 지원',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  '새로운 데이터베이스 레이어는 SQLite, MySQL/MariaDB와 PostgreSQL 데이터베이스를 바로 지원합니다. <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a> 데이터베이스는 추가 모듈을 설치하여 이용할 수 있습니다.',
+  'Better distribution support' =>
+  '더 나은 배포 지원',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  '당신이 제작한 Drupal 시스템을 배포하려면 설치 프로파일을 이용하십시오. 새로운 API와 내보내기가 가능한 설정들을 이용해 설정을 제어할 수 있습니다.',
+  'Extend' =>
+  '확장',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  '커뮤니티의 노력으로 800개 이상의 모듈들이 Drupal 7을 지원하거나 활발히 개발되고 있습니다. <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>은 이미 지원하고 있으며, 매일 더 많은 모듈이 지원하게 될 것입니다.',
+  'See Drupal 7 in action' =>
+  'Drupal 7의 사용 사례를 살펴보십시오',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  'Drupal 7은 이미 수많은 사이트에서 사용하고 있습니다. 주요 사례는 다음과 같습니다:',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '<a href="@examiner-url">Examiner.com</a>',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '<a href="@gardens-url">Drupal Gardens</a>',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '<a href="@subhub-url">SubHub</a>',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '<a href="@cpm-url">Chicago Public Media</a>',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+  'Requirements' =>
+  '요구 사항',
+  'To install Drupal 7, you will need:' =>
+  'Drupal 7을 설치하기 위해서 다음이 필요합니다:',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '<strong>웹서버</strong>: Apache (추천), Nginx, Lighttpd, 또는 Microsoft IIS',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '<strong>데이터베이스</strong>: MySQL 5.0.15 이상, PostgreSQL 8.3 이상, 또는 SQLite 3.x',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '<strong>PHP</strong>: 5.2.4 이상',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '<strong>메모리</strong>: 32MB (여러 모듈을 이용할 경우 64MB 이상의 메모리가 필요할 수 있습니다.)',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  '더 자세한 <a href="@requirements-url">요구 사항 정보</a>.',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  '<a href="@drupalcon-url">DrupalCon Chicago</a>에 참석하여 Drupal 7에 대해 더 알아보십시오.',
+);
