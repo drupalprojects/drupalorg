f3e00ca0fdfa117cf5d0285ad6c8a2a3d59ca14f
tvn <tvn@999848.no-reply.drupal.org>
2011-01-05 09:29:42 +0000

    Chinese traditional

---
+++ drupalorg/translations/drupalorg-d7-announcement.zh-hant.php
@@ -0,0 +1,109 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - 比已往更容易使用，更加強大',
+  'Friendly and powerful:' =>
+  '具有親和力和強大的：',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  '我們很榮幸的為您獻上我們最好的工作成果 &ndash; Drupal 7，這個具有親和力和強大的內容管理平台，讓您能架設任何類型的網站：小至部落格或微博，大至社群及協同合作網站都可以！',
+  'Get started with Drupal 7' =>
+  '由此開始使用 Drupal 7',
+  'Runs on Drupal 7: @site' =>
+  '正在運行 Drupal 7 的網站：@site',
+  'Easier to use' =>
+  '更容易使用',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  '全新打造的管理介面，讓您能輕易完成網站每日的管理工作，尤其是為網站建設與內容編輯者帶來各種友好的改進。',
+  'More flexible' =>
+  '更靈活彈性',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  '定義您想要的內容類型結構，能增加自訂的新欄位至各種不同的文章內容、使用者資訊、回應中，等等。更可透過 800 多個現有的 Drupal 7 模組擴展您的網站功能。',
+  'More scalable' =>
+  '更具擴充性',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  '您的 Drupal 7 網站將會更快速。JavaScript 及 CSS 的最佳化、更好的快取處理機制及更多的性能改進，讓網站反應更快及可處理巨大流量。',
+  'Open source' =>
+  '自由軟體',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  '有道一同，眾志成城。超過一千名聰明及有工作效率的人員一起不停地改進 Drupal 核心、模組、版型及分發安裝功能。',
+  'Take a tour' =>
+  '看一看導覽',
+  'Learn more' =>
+  '學習更多',
+  'Get started with updated documentation for Drupal 7:' =>
+  '閱讀升級文件，開始使用 Drupal 7：',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '<a href="@install-url">安裝 Drupal 7</a>',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '<a href="@upgrade-url">由 Drupal 6 更新</a>',
+  '<a href="@api-url">API Documentation</a>' =>
+  '<a href="@api-url">API 文件</a>',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  '閱讀更多<a href="@documentation-url">線上文件</a>。',
+  'Features' =>
+  '功能',
+  'Flexible content' =>
+  '靈活的內容管理',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  '可以定義自訂欄位至不同的內容類型、使用者檔案、內容回應，分類或其他 Entities 中。儲存這些資料於 SQL、<a href="@nosql-url">NoSQL</a>，或遠端儲存裝置。',
+  'Better theming' =>
+  '更好的版型機制',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  '透過全新的 Render API 及底層的 Hooks ，完全掌控在螢幕上能看到的元素。新的 RDF 模組更提供了語意網站標籤解決方案。',
+  'Accessible' =>
+  '無障礙設計',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  '管理員界面現在更易於使用了！大量前端設計的改進令你更易打造無障礙設計網站。',
+  'Images and files' =>
+  '圖片與檔案',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  '內建圖片處理，可任意新增至內容中。產生不同的縮圖、預覽圖或各種圖片效果樣式。改進檔案處理，私人檔案與公開檔案現在可以同時使用。',
+  'Automated code testing' =>
+  '自動化程式碼測試',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  '全新的自動化測試架構，內建超過 30,000 個測試單元，允許為 Drupal 核心及社群貢獻模組進行<a href="@qa-url">持續性的整合測試及修正</a>工作。',
+  'Improved database support' =>
+  '改進資料庫支援',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  '新的資料庫層提供 SQLite、MySQL/MariaDB 及 PostgreSQL 支援。更可透過安裝新的模組，使用 <a href="@sqlsrv-url">MS SQL Server</a>或<a href="@oracle-url">Oracle</a>等等',
+  'Better distribution support' =>
+  '更好的分發安裝支援',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  '透過安裝資料檔分發您自訂的 Drupal 產品。全新的 API 及可匯出設定檔，讓您從程式碼中取得更多設定值。',
+  'Extend' =>
+  '擴展性',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  '感謝強大的社群力量！現已有超過 800 個可使用及開發中的 Drupal 7 模組，比如：<a href="@views-url">Views</a>、<a href="@pathauto-url">Pathauto</a>及<a href="@wysiwyg-url">WYSIWYG</a>，更多更多正在加入中...',
+  'See Drupal 7 in action' =>
+  '看一看 Drupal 7 ',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  'Drupal 7 已經使用於百多個網站，包括：',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '<a href="@examiner-url">Examiner.com</a>',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '<a href="@gardens-url">Drupal Gardens</a>',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '<a href="@subhub-url">SubHub</a>',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '<a href="@cpm-url">Chicago Public Media</a>',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+  'Requirements' =>
+  '需求',
+  'To install Drupal 7, you will need:' =>
+  '安裝 Drupal 7，您將需要：',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '<strong>網站伺服器</strong>: Apache (建議), Nginx, Lighttpd, 或 Microsoft IIS',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '<strong>資料庫</strong>: MySQL 5.0.15 或更高、PostgreSQL 8.3 或更高、SQLite 3.x',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '<strong>PHP</strong>: 5.2.4 或更高',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '<strong>記憶體</strong>：32MB (網站使用了多個常用模組，將需要 64MB 或更多。)',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  '更多 <a href="@requirements-url">安裝需求</a>資料。',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  '參與<a href="@drupalcon-url">DrupalCon Chicago</a> 了解更多 Drupal 7 的功能。',
+);
