ef1b9ebded86951ce0ab76066829983b9970f7ff
tvn <tvn@999848.no-reply.drupal.org>
2011-01-05 13:17:19 +0000

    Norwegian (Bokmal)

---
+++ drupalorg/translations/drupalorg-d7-announcement.nb.php
@@ -0,0 +1,109 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - Enklere og kraftigere enn noen gang før',
+  'Friendly and powerful:' =>
+  'Vennlig og kraftig:',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  'Vi er stolte av å presentere deg vårt beste arbeid hittil &ndash; Drupal 7, den vennlige og kraftfulle publiseringsplattform som kan brukes til å bygge nesten alle slags nettsteder: fra blogger og mikronettsteder for sosiale fellesskap.',
+  'Get started with Drupal 7' =>
+  'Kom i gang med Drupal 7',
+  'Runs on Drupal 7: @site' =>
+  'Kjører på Drupal 7: @site',
+  'Easier to use' =>
+  'Enklere å bruke',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  'Et helt nytt administrativt grensesnitt gjør det enklere å løse dine daglige oppgaver. Det ble lagt inn mange forbedringer spesielt for nettstedsadministratorer og redaktører.',
+  'More flexible' =>
+  'Mer fleksibel',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  'Bestem din egen innholdsstruktur og legge til egendefinerte felt for innhold, brukere, kommentarer m.m. Utvid ditt nettsted med en av over 800 moduler som allerede er tilgjengelige for Drupal 7.',
+  'More scalable' =>
+  'Mer skalérbar',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  'Ditt Drupal 7-nettsted vil være rask og i stand til å håndtere store volumer av trafikk takket være bedre JavaScript- og CSS-optimalisering, bedre caching, m.m.',
+  'Open source' =>
+  'Åpen kilde',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  'Come for the software, stay for the community. Tusenvis av intelligente og produktive mennesker arbeider sammen for å kontinuerlig forbedre Drupal, moduler, utseendemaler og distribusjoner.',
+  'Take a tour' =>
+  'Ta en tur',
+  'Learn more' =>
+  'Lær mer',
+  'Get started with updated documentation for Drupal 7:' =>
+  'Kom i gang med oppdatert dokumentasjon for Drupal 7:',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '<a href="@install-url">Installering av Drupal 7</a>',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '<a href="@upgrade-url">Oppgradering fra versjon 6</a>',
+  '<a href="@api-url">API Documentation</a>' =>
+  '<a href="@api-url">API-dokumentasjon</a>',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  'Flere emner i <a href="@documentation-url">online-dokumentasjonen</a>.',
+  'Features' =>
+  'Funksjonaliteter',
+  'Flexible content' =>
+  'Fleksibelt innhold',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  'Opprett egendefinerte felt som kan brukes på tvers av innholdstyper, brukere, kommentarer, termer og andre entititer. Lagre feltenes data i SQL, <a href="@nosql-url">NoSQL</a> eller bruk ekstern lagring',
+  'Better theming' =>
+  'Bedre theming',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  'Bestem nøyaktig hva som vises hvor på skjermen med den nye Render API og en hel rekke nye alter hooks. Den nye RDF-modulen leverer semantisk web-markup.',
+  'Accessible' =>
+  'Tilgjengelig',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  'Administrasjonssidene er nå mye mer tilgjengelig. Mange forbedringer i grensesnittet gjør det enklere for deg å bygge tilgjengelige nettsteder.',
+  'Images and files' =>
+  'Bilder og filer',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  'Å legge bilder til innhold er nå bygget. Opprett forskjellige versjoner av miniatyrbildene, forhåndsvisning og andre bildestiler. Private og offentlige filer kan nå brukes samtidig.',
+  'Automated code testing' =>
+  'Automatisert testing av koden',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  'Et nytt automatisert test-rammeverk med over 30.000 innebygde tester gjør det mulig å gjøre <a href="@qa-url">kontinuerlige integreringstesting</a> av alle patcher til Drupal-kjerne og tredjepartsmoduler.',
+  'Improved database support' =>
+  'Forbedret database-support',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  'Et nytt databaselag gir direkte støtte til SQLite, MySQL/MariaDB og PostgreSQL. Installér tredjepartsmoduler for å bruke <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, m.m.',
+  'Better distribution support' =>
+  'Bedre støtte av distribusjoner',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  'Bruk installasjonsprofiler til å distribuere ditt Drupal-basert produkt. Et nytt API og eksportérbare konfigurasjoner gjør det mulig å lagre flere innstillinger i kode.',
+  'Extend' =>
+  'Utvid',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  'Takket være innsatsen fra fellesskapet er mer enn 800 moduler tilgjengelig eller under utvikling for Drupal 7, inkludert <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, og flere kommer hver dag.',
+  'See Drupal 7 in action' =>
+  'Se Drupal 7 i bruk',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  'Drupal 7 er allerede i bruk på hundrevis av nettsteder. For eksempel:',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '<a href="@examiner-url">Examiner.com</a>',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '<a href="@gardens-url">Drupal Gardens</a>',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '<a href="@subhub-url">SubHub</a>',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '<a href="@cpm-url">Chicago Public Media</a>',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+  'Requirements' =>
+  'Systemkrav',
+  'To install Drupal 7, you will need:' =>
+  'For å installere Drupal 7, trenger du:',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '<strong>Web Server</strong>: Apache (anbefales), Nginx, Lighttpd, or Microsoft IIS',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '<strong>Database</strong>: MySQL 5.0.15 eller nyere, PostgreSQL 8.3 eller nyere, eller SQLite 3.x',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '<strong>PHP</strong>: 5.2.4 eller nyere',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '<strong>Minne</strong>: 32MB (Et nettsted med en rekke vanlige aktiverte moduler kan kreve 64MB minne eller mer.)',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  'Mer <a href="@requirements-url">info om systemkrav</a>.',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  'Reis til og delta på <a href="@drupalcon-url">DrupalCon Chicago</a> for å lære mer om Drupal 7',
+);
