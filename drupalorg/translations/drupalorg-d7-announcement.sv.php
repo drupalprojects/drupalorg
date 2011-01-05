16d450ca543e2eb0b26292d50ad7cbe31a907fcb
tvn <tvn@999848.no-reply.drupal.org>
2011-01-05 18:40:30 +0000

    Swedish translation

---
+++ drupalorg/translations/drupalorg-d7-announcement.sv.php
@@ -0,0 +1,111 @@
+<?php
+
+$strings = array(
+
+'Drupal 7 - Easier and more powerful than ever' => 'Drupal 7 - Mer lättanvänt och kraftfullare än någonsin',
+
+'Friendly and powerful:' => 'Enkel och kraftfullt',
+
+'Drupal 7' => 'Drupal 7',
+
+'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' => 'Vi är stolta över att presentera vårt bästa verk hittils &ndash; Drupal 7, den lättanvända och kraftfulla publiceringsplattformen för att bygga i princip vilken sajt som helst: från bloggar och microsajter till kollaborativa sociala commynitys.',
+
+'Get started with Drupal 7' => 'Börja med Drupal 7',
+
+'Runs on Drupal 7: @site' => 'Använder Drupal 7: @site',
+
+'Easier to use' => 'Lättare att använda',
+
+'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' => 'Ett omarbetat administrationsgränssnitt gör dina dagliga uppgifter lättare att utföra. Många förbättringar har lagts till för sajtbyggare och redaktörer',
+
+'More flexible' => 'Mer flexibelt',
+
+'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' => 'Definera din egen innehållsstruktur och lägg till egna fält på innehåll, användare, kommentarer och mer. Bygg ut din sajt med någon av de 800 moduler som finns tillgängliga för Drupal 7.',
+
+'More scalable' => 'Mer skalbart',
+
+'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' => 'Din sajt på Drupal 7 blir snabb, responsiv och kan hantera enorm trafik tack vare förbättrad Javascript- och CSS-optimering, bättre caching med mera.',
+
+'Open source' => 'Öppen källkod',
+
+'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' => 'Kom för mjukvaran, stanna för gemenskapen. Tusentals smarta och produktiva människor arbetar ihop för att ständigt förbättra Drupal, moduler, teman och distributioner.',
+
+'Take a tour' => 'Ta en guidad tur',
+
+'Learn more' => 'Läs mer',
+
+'Get started with updated documentation for Drupal 7:' => 'Kom igång med uppdaterad dokumentation för Drupal 7',
+
+'<a href="@install-url">Installing Drupal 7</a>' => '<a href="@install-url">Installera Drupal 7</a>',
+
+'<a href="@upgrade-url">Upgrading from version 6</a>' => '<a href="@upgrade-url">Uppgradera från version 6</a>',
+
+'<a href="@api-url">API Documentation</a>' => '<a href="@api-url">API-dokumentation</a>',
+
+'More topics in the <a href="@documentation-url">online documentation</a>.' => 'Fler ämnen i <a href="@documentation-url">dokumentationen online</a>.',
+
+'Features' => 'Features',
+
+'Flexible content' => 'Flexibelt innehåll',
+
+'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' => 'Definera egna fält som kan användas över innehållstyper, användare, kommentarer, termer och andra entiteter. Lagra datan för fälten i SQL, <a href="@nosql-url">NoSQL</a>, eller använd extern lagring.',
+
+'Better theming' => 'Bättre utseendeanpassning',
+
+'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' => 'Kontrollera exakt vad som visas vart på skärmen med ett nytt API för rendering och några riktigt radikala altereringsmöjligheter. Den nya RDF modulen tillhandahåller semantisk markup.',
+
+'Accessible' => 'Tillgängligt',
+
+'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' => 'Administrationsvyer är mycket enklare att komma åt. Många förbättringar i front-end gör det enklare för dig att bygga åtkomliga sajter.',
+
+'Images and files' => 'Bilder och filer',
+
+'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' => 'Att lägga till bilder till innehåll är nu inbyggt. Generera olika versioner av tumnaglar, förhandsvisningar och andra bildstilar. Privat filhantering kan nu ske sida vid sida med publika filer.',
+
+'Automated code testing' => 'Automatiserad testning av kod',
+
+'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' => 'Ett nytt ramverk med över 30,000 automatiserade tester för <a href="@qa-url">löpande integrationstestning</a> av alla patchar till kärnan och tillägsmoduler.',
+
+'Improved database support' => 'Bättre databasstöd',
+
+'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' => 'Ett nytt abstraktionslager för databasen ger inbyggt stöd för SQLite, MySQL/MariaDB och PostgreSQL. Installera tilläggsmoduler för att använda <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, med flera.',
+
+'Better distribution support' => 'Bättre distributionsstöd',
+
+'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' => 'Använd installationsprofiler för att disitibuera din anpassade Drupal-produkt. Ett nytt API och exporterbar konfiguration låter dig spara ner fler inställningar i kod.',
+
+'Extend' => 'Utöka',
+
+'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' => 'Tack vare den stora insatsen från communityt så finns fler än 800 moduler tillgängliga eller under aktiv utveckling för Drupal 7. Bland dem finns <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, och <a href="@wysiwyg-url">WYSIWYG</a>, med fler på väg varje dag.',
+
+'See Drupal 7 in action' => 'Sajter på Drupal 7',
+
+'Drupal 7 is already in use on hundreds of sites. Some examples include:' => 'Drupal 7 används redan av hundratals sajter. Några exempel inkluderar:',
+
+'<a href="@examiner-url">Examiner.com</a>' => '<a href="@examiner-url">Examiner.com</a>',
+
+'<a href="@gardens-url">Drupal Gardens</a>' => '<a href="@gardens-url">Drupal Gardens</a>',
+
+'<a href="@subhub-url">SubHub</a>' => '<a href="@subhub-url">SubHub</a>',
+
+'<a href="@cpm-url">Chicago Public Media</a>' => '<a href="@cpm-url">Chicago Public Media</a>',
+
+'<a href="@sagmeister-url">Stefan Sagmeister</a>' => '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+
+'Requirements' => 'Kravspecifikation',
+
+'To install Drupal 7, you will need:' => 'För att installera Drupal 7 behöver du:',
+
+'<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' => '<strong>Webbserver</strong>: Apache (rekommenderad), Nginx, Lighttpd, eller Microsoft IIS',
+
+'<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' => '<strong>Databas</strong>: MySQL 5.0.15 eller senare, PostgreSQL 8.3 eller senare, eller SQLite 3.x',
+
+'<strong>PHP</strong>: 5.2.4 and higher' => '<strong>PHP</strong>: 5.2.4 eller senare',
+
+'<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' => '<strong>Minne</strong>: 32 MB (En sajt med ett antal vanliga moduler aktiverade kan kräva 64 MB minne eller mer.)',
+
+'More <a href="@requirements-url">requirements information</a>.' => 'Mer om <a href="@requirements-url">kravspecifikationer</a>.',
+
+'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' => 'Besök <a href="@drupalcon-url">DrupalCon Chicago</a> för att lära dig mer om Drupal 7',
+
+);
