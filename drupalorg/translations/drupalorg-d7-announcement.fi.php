ea5c3c3a63ba126cc48735c0cfdb2e2026fce872
tvn <tvn@999848.no-reply.drupal.org>
2011-01-05 09:46:07 +0000

    Finnish translation

---
+++ drupalorg/translations/drupalorg-d7-announcement.fi.php
@@ -0,0 +1,109 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - Helppokäyttöisempi ja mahtavampi kuin koskaan',
+  'Friendly and powerful:' =>
+  'Tehokas ja miellyttävä:',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  'Ylpeinä esittelemme loistavan työmme tuloksen: Drupal 7, helppokäyttöinen ja tehokas sisällönhallintajärjestelmä jolla rakennat kaikenlaisia verkkopalveluita aina pienistä kampanjasivustoista isoihin sosiaalisiin verkkoyhteisöihin.',
+  'Get started with Drupal 7' =>
+  'Ota käyttöösi Drupal 7',
+  'Runs on Drupal 7: @site' =>
+  'Drupal 7 käytössä: @site',
+  'Easier to use' =>
+  'Helppokäyttöisempi',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  'Täydellisesti uusitut hallintasivut helpottavat päivittäisten tehtävien suorittamisessa. Lukuisia parannuksia on tehty etenkin sisällönsyöttäjien ja sivuston rakentajien työn helpottamiseksi.',
+  'More flexible' =>
+  'Moukattavampi',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  'Määrittele itse sopivia artikkelityyppejä ja lisää haluamiasi tietokenttiä liitettäväksi artikkeleihin, käyttäjiin, kommentteihin sekä muihin sisältöihin. Laajenna sivustosi toiminnallisuuksia moduulein, joita löytyy jo yli 800 Drupal 7:lle.',
+  'More scalable' =>
+  'Skaalautuvampi',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  'Drupal 7 sivustosi tulee olemaan nopea ja suuria kävijämääriä palveleva, muun muassa parennettujen JavaScript- ja CSS-optimointien sekä parannettujen välimuistitusratkaisujen ansiosta.',
+  'Open source' =>
+  'Avoin lähdekoodi',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  'Tule hakeaksesi toimiva ohjelmisto, niin tulet osaksi isoa yhteisöä. Tuhannet fiksut ja tuotteliaat ihmiset työskentelevät jatkuvasti yhdessä parantaakseen Drupal-järjestelmää.',
+  'Take a tour' =>
+  'Tee tutusmiskierros',
+  'Learn more' =>
+  'Opi lisää',
+  'Get started with updated documentation for Drupal 7:' =>
+  'Aloita näistä Drupal 7 -päivitetyistä ohjeista:',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '<a href="@install-url">Drupal 7:n asentaminen</a>',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '<a href="@upgrade-url">Päivittäminen versiosta 6</a>',
+  '<a href="@api-url">API Documentation</a>' =>
+  '<a href="@api-url">API-dokumentaatio</a>',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  'Lisää aiheita <a href="@documentation-url">online-dokumentaatiossa</a>.',
+  'Features' =>
+  'Ominaisuudet',
+  'Flexible content' =>
+  'Joustavat sisällöt',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  'Määrittele itse tietokenttiä, joita voidaan käyttää eri sivutyypeissä, käyttäjätiedoissa, luokitteluissa ja muissa sisällöissä. Tietojen tallennuksessa voi olla käytössä SQL, <a href="@nosql-url">NoSQL</a> tai etäpalvelin.',
+  'Better theming' =>
+  'Tarkka ulkoasumäärittely',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  'Kontrolloi tarkasti mitä sivuilla näytetään missäkin kohdin käyttäen uutta Render API:a ja voimakkaita muokkausmahdollisuuksia. RDF-moduuli tarjoilee semattisen tavan sisältöjen esittämiseen.',
+  'Accessible' =>
+  'Saavutettavuus',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  'Ylläpidolliset näkymät ovat nyt entistä saavutettavammat. Useiden käyttöliittymäparannukset tekevät helpoksi saavutettavien sivustojen rakentamisen.',
+  'Images and files' =>
+  'Kuvat ja tiedostot',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  'Kuvien lisääminen sisältöihin on suoraan valmis käytettäväksi. Erilaiset määrittely automaattsesti luotaviin esikatselu- ja muihin kuvaversioihin. Yksityisten tiedostojen käsittely on mahdollista julkisten tiedostojen rinnalla.',
+  'Automated code testing' =>
+  'Automatisoitu kooditestaus',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  'Automatisoitu testausjärjestelmä, jossa on yli 30000 sisäänrakennettua testiä suorittamaan <a href="@qa-url">jatkuvaa integraatiotestausta</a> muutoksille Drupalin koodissa ja laajennusmoduuleissa.',
+  'Improved database support' =>
+  'Parannettu tietokantatuki',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  'Uuden tietokantakerroksen myötä valmis tuki SQLite-, MySQL-/MariaDB- ja PostgreSQL-tietokannoille. <a href="@sqlsrv-url">MS SQL Server</a> ja <a href="@oracle-url">Oracle</a> ovat käytettävissä laajennusmoduuliensa avulla.',
+  'Better distribution support' =>
+  'Parannettu tuki jakamiseen',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  'Asennusprofiileita käyttäen voit jakaa räätälöimäsi Drupal-tuotteen. Myös uusi rajapinta ja koodin kautta vietävissä olevat konfiguraatiot tehostavat jakamista.',
+  'Extend' =>
+  'Laajennusmahdollisuudet',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  'Laajan yhteisön tekemä panostus näkyy yli 800 moduulissa, joista on jo Drupal 7 -versiot. Tähän alati kasvavaan listaan kuuluvat muun muassa <a href="@views-url">Views</a>-, <a href="@pathauto-url">Pathauto</a>- ja <a href="@wysiwyg-url">WYSIWYG</a>-moduulit.',
+  'See Drupal 7 in action' =>
+  'Näe Drupal 7 käytössä',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  'Drupal 7 on jo käytössä sadoilla sivustoilla. Tässä muutama esimerkki:',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '<a href="@examiner-url">Examiner.com</a>',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '<a href="@gardens-url">Drupal Gardens</a>',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '<a href="@subhub-url">SubHub</a>',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '<a href="@cpm-url">Chicago Public Media</a>',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+  'Requirements' =>
+  'Järjestelmävaatimukset',
+  'To install Drupal 7, you will need:' =>
+  'Asennusta varten Drupal 7 tarvitsee seuraavat valmiudet:',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '<strong>WWW-palvelin</strong>: Apache (suositeltu), Nginx, Lighttpd tai Microsoft IIS',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '<strong>Tietokanta</strong>: MySQL 5.0.15 tai uudempi, PostgreSQL 8.3 tai uudempi, SQLite 3.x',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '<strong>PHP</strong>: 5.2.4 tai uudempi',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '<strong>Muisti</strong>: 32Mt (Sivusto, jolla useita moduuleita käytössä 64Mt tai enemmän.)',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  'Lisätietoa <a href="@requirements-url">järjestelmävaatimukset-sivulta</a>.',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  'Osallistu <a href="@drupalcon-url">DrupalCon Chicago</a> -tapahtumaan oppiaksesi lisää Drupal 7:stä',
+);
