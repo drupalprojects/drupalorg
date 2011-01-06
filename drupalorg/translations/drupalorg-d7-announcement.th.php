c59e1423480aa51e40320b0030841f4fe7cec3c6
tvn <tvn@999848.no-reply.drupal.org>
2011-01-06 10:48:59 +0000

    Thai translation

---
+++ drupalorg/translations/drupalorg-d7-announcement.th.php
@@ -0,0 +1,109 @@
+<?php
+$strings = array(
+  'Drupal 7 - Easier and more powerful than ever' =>
+  'Drupal 7 - ใช้งานง่ายและมีประสิทธิภาพมากกว่าเคย',
+  'Friendly and powerful:' => 
+     'เป็นมิตรและมีประสิทธิภาพ:',
+  'Drupal 7' =>
+  'Drupal 7',
+  'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
+  'เรายินดีที่จะแสดงผลงานที่ดีที่สุดให้คุณเห็น &ndash; Drupal 7, แพลตฟอร์มจัดการเนื้อหาที่เป็นมิตรและมีประสิทธิภาพในการสร้างเว็บไซต์เกือบทุกประเภท ตั้งแต่บล็อกจนถึงเว็บเครือข่ายสังคม',
+  'Get started with Drupal 7' =>
+  'เริ่มต้นกับ Drupal 7',
+  'Runs on Drupal 7: @site' =>
+  'ทำงานบน Drupal 7: @site',
+  'Easier to use' =>
+  'ใช้งานง่ายขึ้น',
+  'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
+  'อินเตอร์เฟซในการบริหารเว็บไซต์ที่ปรับปรุงใหม่หมดทำให้ชีวิตคุณง่ายขึ้น สิ่งที่ปรับปรุงใหม่นั้นถูกออกแบบมาสำหรับทุกฝ่ายไม่ว่าคุณจะเป็นคนสร้างเว็บไซต์หรือคนแก้ไขเนื้อหาในเว็บนั้น',
+  'More flexible' =>
+  'ยืดหยุ่นมากขึ้น',
+  'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
+  'กำหนดโครงสร้างเนื้อหาของคุณ และเพิ่มฟีลด์เนื้อหา ผู้ใช้ คอมเมนต์และอื่นๆ ได้อย่างง่ายดาย เพิ่มคุณสมบัติของเว็บไซต์โดยใส่โมดูลกว่า 800 ตัวที่พร้อมใช้สำหรับ Drupal 7',
+  'More scalable' =>
+  'รองรับการขยายตัวที่มากขึ้น',
+  'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
+  'เว็บไซต์ Drupal 7 ของคุณจะเร็ว ตอบสนองทันที และรองรับการเข้าชมจำนวนมาก ด้วยคุณสมบัติที่ปรับปรุงของการประมวล JavaScript และ CSS รวมถึงระบบแคชและอีกหลายความสามารถ',
+  'Open source' =>
+  'โอเพนซอร์ซ',
+  'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
+  'ต้องการซอฟต์แวร์และสถิตอยู่กับชุมชน มีคนที่มีความสามารถหลายพันคนที่ช่วยกันสร้างและพัฒนา โมดูล และธีม อยู่อย่างต่อเนื่อง'',
+  'Take a tour' =>
+  'ลองชม',
+  'Learn more' =>
+  'เรียนรู้เพิ่มเติม',
+  'Get started with updated documentation for Drupal 7:' =>
+  'เริ่มต้นรู้จักกับคู่มือสำหรับ Drupal 7',
+  '<a href="@install-url">Installing Drupal 7</a>' =>
+  '<a href="@install-url">การติดตั้ง Drupal 7</a>',
+  '<a href="@upgrade-url">Upgrading from version 6</a>' =>
+  '<a href="@upgrade-url">การอัปเกรดจากรุ่น 6</a>',
+  '<a href="@api-url">API Documentation</a>' =>
+  '<a href="@api-url">คู่มือ API</a>',
+  'More topics in the <a href="@documentation-url">online documentation</a>.' =>
+  'หัวข้ออื่นใน<a href="@documentation-url">คู่มือออนไลน์</a>.',
+  'Features' =>
+  'คุณสมบัติ',
+  'Flexible content' =>
+  'เนื้อหาที่ยืดหยุ่น',
+  'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
+  'กำหนดฟีลด์ขึ้นมาสำหรับเนื้อหา ผู้ใช้ คอมเมนต์ คำจำกัดความ และส่วนประกอบอื่น โดยเก็บข้อมูลเหล่านี้ลงใน SQL, <a href="@nosql-url">NoSQL</a>, หรือที่เก็บระยะไกล',
+  'Better theming' =>
+  'ธีมที่ดีขึ้น',
+  'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
+  'ควบคุมสิ่งที่จะแสดงผลด้วย Render API และฮุกตัวใหม่ รวมถึงโมดูล RDF ตัวใหม่ที่ช่วยจัดการมาร์กอัปสำหรับเว็บซีแมนติก',
+  'Accessible' =>
+  'การเข้าถึง',
+  'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
+  'หน้าจอแอดมินสามารถเข้าถึงได้ง่ายขึ้น ได้มีการปรับปรุงอินเตอร์เฟซให้ใช้งานง่ายขึ้นเพื่อที่จะสร้างเว็บไซต์ให้เข้าถึงได้ง่ายขึ้น',
+  'Images and files' =>
+  'ภาพและไฟล์',
+  'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
+  'การเพิ่มภาพเข้าไปในเนื้อหาได้รวมอยู่ในแกนหลัก การสร้างธัมบ์เนล พรีวิว และสไตล์ภาพต่างๆ  การจัดการไฟล์ส่วนตัวสามารถทำงานควบคู่ไปกับไฟล์ที่เปิดให้เป็นสาธารณะ',
+  'Automated code testing' =>
+  'การทดสอบโค้ดอัตโนมัติ',
+  'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
+  'เฟรมเวิร์กสำหรับการทดสอบอัตโนมัติทดสอบมากกว่า 30,000 ตัว สามารถ<a href="@qa-url">การทดสอบอย่างต่อเนื่อง</a> สำหรับไฟล์แกนของ Drupal และโมดูลหลัก',
+  'Improved database support' =>
+  'การรับรองฐานข้อมูลที่พัฒนาขึ้น',
+  'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
+  'เลเยอร์ฐานข้อมูลใหม่ทำให้สามารถรองรับ SQLite, MySQL/MariaDB และ PostgreSQL ได้ทันทีโดยไม่ต้องติดตั้งตัวเสริม หรือสามารถติดตั้งเพิ่มเพื่อใช้งาน <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a> และตัวอื่น',
+  'Better distribution support' =>
+  'การรับรองการแจกจ่ายที่ดีขึ้น',
+  'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
+  'การใช้งานโพรไฟล์การติดตั้งเพื่อแจกจ่าย Drupal ตัวปรับปรุง ด้วยคุณสมบัติใหม่ของ API และการตั้งค่าการส่งออกนั้นสามารถให้คุณจัดการโพรไฟล์ได้ภายในการตั้งค่าในตัวโค้ดได้ทันที',
+  'Extend' =>
+  'การขยายตัว',
+  'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
+  'ขอบคุณสำหรับความร่วมมือของชุมชน โมดูลมากกว่า 800 ตัวที่พร้อมใช้งานสำหรับ Drupal 7 และอีกมากมายที่อยู่ในระหว่างการพัฒนา รวมถึง <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a> และ <a href="@wysiwyg-url">WYSIWYG</a> และอีกจำนวนมากที่เพิ่มขึ้นทุกวัน',
+  'See Drupal 7 in action' =>
+  'มาดูของจริงกับ Drupal 7',
+  'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
+  'Drupal 7 ถูกนำมาใช้กับเว็บไซต์หลายร้อยเว็บ ยกตัวอย่างเช่น:',
+  '<a href="@examiner-url">Examiner.com</a>' =>
+  '<a href="@examiner-url">Examiner.com</a>',
+  '<a href="@gardens-url">Drupal Gardens</a>' =>
+  '<a href="@gardens-url">Drupal Gardens</a>',
+  '<a href="@subhub-url">SubHub</a>' =>
+  '<a href="@subhub-url">SubHub</a>',
+  '<a href="@cpm-url">Chicago Public Media</a>' =>
+  '<a href="@cpm-url">Chicago Public Media</a>',
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>' =>
+  '<a href="@sagmeister-url">Stefan Sagmeister</a>',
+  'Requirements' =>
+  'ความต้องการ',
+  'To install Drupal 7, you will need:' =>
+  'เพื่อที่จะติดตั้ง Drupal 7 คุณจำเป็นต้อง',
+  '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
+  '<strong>เว็บเซิร์ฟเวอร์</strong>: Apache (แนะนำ), Nginx, Lighttpd หรือ Microsoft IIS',
+  '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
+  '<strong>ฐานข้อมูล</strong>: MySQL 5.0.15 หรือสูงกว่า, PostgreSQL 8.3 หรือสูงกว่า, หรือ SQLite 3.x',
+  '<strong>PHP</strong>: 5.2.4 and higher' =>
+  '<strong>PHP</strong>: 5.2.4 หรือสูงกว่า',
+  '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
+  '<strong>ความจำ</strong>: 32MB (เว็บที่โมดูลมากอาจจะต้องการ 64 MB หรือมากกว่า)',
+  'More <a href="@requirements-url">requirements information</a>.' =>
+  'มากกว่านี้ <a href="@requirements-url">ข้อมูลความต้องการของระบบ</a>',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  'เข้าร่วมสัมมนา <a href="@drupalcon-url">DrupalCon Chicago</a> ทีเมืองชิคาโกเพื่อเรียนรู้เพิ่มเติมเกี่ยวกับ Drupal 7',
+);
