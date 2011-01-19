15c0c1d2dc034b66edc51c3fbfeb1d6cac2309cc
tvn <tvn@999848.no-reply.drupal.org>
2011-01-19 07:25:21 +0000

    Croatian translation in proper coding.

--- drupalorg/translations/drupalorg-d7-announcement.hr.php
+++ drupalorg/translations/drupalorg-d7-announcement.hr.php
@@ -1,39 +1,39 @@
 <?php
 $strings = array(
   'Drupal 7 - Easier and more powerful than ever' =>
-  'Drupal 7 - Jednostavniji i ucinkovitiji nego ikad',
+  'Drupal 7 - Jednostavniji i učinkovitiji nego ikad',
   'Friendly and powerful:' =>
-  'Prijateljski i ucinkovit:',
+  'Prijateljski i učinkovit:',
   'Drupal 7' =>
   'Drupal 7',
   'We are proud to present to you our best work yet &ndash; Drupal 7, the friendly and powerful content management platform for building nearly any kind of website: from blogs and micro-sites to collaborative social communities.' =>
-  'S ponosom Vam predstavljamo nas do sada najbolji uradak: Drupal 7, pristupacnu i ucinkovitu platformu za izgradnju gotovo svih vrsta web sjedista: od blogova i mikro-sjedista do sjedista namijenjenih suradnji drustvenih zajednica.',
+  'S ponosom Vam predstavljamo naš do sada najbolji uradak: Drupal 7, pristupačnu i učinkovitu platformu za izgradnju gotovo svih vrsta web sjedišta: od blogova i mikro-sjedišta do sjedišta namijenjenih suradnji društvenih zajednica.',
   'Get started with Drupal 7' =>
-  'Zapocnite s Drupalom 7',
+  'Započnite s Drupalom 7',
   'Runs on Drupal 7: @site' =>
-  'S Drupalom 7 izgradeni su: @site',
+  'S Drupalom 7 izgrađeni su: @site',
   'Easier to use' =>
   'Jednostavniji',
   'An entirely revamped administrative interface makes your daily tasks easier to find and carry out. Many improvements were added specifically for site builders and content editors.' =>
-  'Potpuno preuredeno administracijsko sucelje pojednostavit ce Vase svakodnevne zadatke. Mnoga poboljsanja namijenjena su ponajvise osobama koje razvijaju web sjedista te urednicima web sadrzaja.',
+  'Potpuno preuređeno administracijsko sučelje pojednostavit će Vaše svakodnevne zadatke. Mnoga poboljšanja namijenjena su ponajviše osobama koje razvijaju web sjedišta te urednicima web sadržaja.',
   'More flexible' =>
   'Prilagodljiviji',
   'Define your own content structure and add custom fields to content, users, comments, and more. Extend your site with one of the over 800 modules already available for Drupal 7.' =>
-  'Uspostavite vlastitu strukturu dokumenata prilagodivsi ju proizvoljnim poljima u prikazu sadrzaja, korisnika, komentara itd. Prosirite svoje web sjediste izborom vise od 800 vec dostupnih modula za Drupal 7.',
+  'Uspostavite vlastitu strukturu dokumenata prilagodivši ju proizvoljnim poljima u prikazu sadržaja, korisnika, komentara itd. Proširite svoje web sjedište izborom više od 800 već dostupnih modula za Drupal 7.',
   'More scalable' =>
   'Skalabilniji',
   'Your Drupal 7 site will be fast, responsive and handle huge amounts of traffic thanks to improved JavaScript and CSS optimization, better caching and more.' =>
-  'Vase Drupal 7 web sjediste bit ce brze, brzeg odziva i lakse ce podrzavati veliki opseg prometa zahvaljujuci JavaScript i CSS optimizaciji, boljem koristenju privremene memorije i drugim unapredenjima.',
+  'Vaše Drupal 7 web sjedište bit će brže, bržeg odziva i lakše će podržavati veliki opseg prometa zahvaljujući JavaScript i CSS optimizaciji, boljem korištenju privremene memorije i drugim unapređenjima.',
   'Open source' =>
   'Otvorenog koda',
   'Come for the software, stay for the community. Thousands of smart and productive people work together to continuously improve Drupal, modules, themes and distributions.' =>
-  'Dodite zbog softvera, ostanite zbog zajednice. Tisuce pametnih i produktivnih ljudi neprestano rade zajedno na poboljsanju Drupala, modula, tema i distribucija.',
+  'Dođite zbog softvera, ostanite zbog zajednice. Tisuće pametnih i produktivnih ljudi neprestano rade zajedno na poboljšanju Drupala, modula, tema i distribucija.',
   'Take a tour' =>
   'Upoznajte Drupal 7',
   'Learn more' =>
-  'Naucite vise',
+  'Naučite više',
   'Get started with updated documentation for Drupal 7:' =>
-  'Zapocnite s azuriranom dokumentacijom za Drupal 7:',
+  'Započnite s ažuriranom dokumentacijom za Drupal 7:',
   '<a href="@install-url">Installing Drupal 7</a>' =>
   '<a href="@install-url">Instalacija Drupala 7</a>',
   '<a href="@upgrade-url">Upgrading from version 6</a>' =>
@@ -41,45 +41,45 @@ $strings = array(
   '<a href="@api-url">API Documentation</a>' =>
   '<a href="@api-url">API dokumentacija</a>',
   'More topics in the <a href="@documentation-url">online documentation</a>.' =>
-  'Vise sadrzaja u <a href="@documentation-url">online dokumentaciji</a>.',
+  'Više sadržaja u <a href="@documentation-url">online dokumentaciji</a>.',
   'Features' =>
-  'Mogucnosti',
+  'Mogućnosti',
   'Flexible content' =>
-  'Prilagodljiviji sadrzaj',
+  'Prilagodljiviji sadržaj',
   'Define custom fields that can be used across content types, users, comments, terms and other entities. Store the data for these fields in SQL, <a href="@nosql-url">NoSQL</a>, or use remote storage.' =>
-  'Definirajte proizvoljna polja koja cete koristiti u radu s razlicitim vrstama sadrzaja, korisnicima, komentarima, terminima i drugim entitetima. Spremite podatke o poljima u SQL, <a href="@nosql-url">NoSQL</a> ili u udaljena spremista podataka.',
+  'Definirajte proizvoljna polja koja ćete koristiti u radu s različitim vrstama sadržaja, korisnicima, komentarima, terminima i drugim entitetima. Spremite podatke o poljima u SQL, <a href="@nosql-url">NoSQL</a> ili u udaljena spremišta podataka.',
   'Better theming' =>
   'Bolja prilagodba tema',
   'Control exactly what gets shown where on the screen with the new Render API and some truly radical alter hooks. The new RDF module provides semantic web markup.' =>
-  'Upravljajte tocno s onim sto i gdje ce biti prikazano na ekranu zahvaljujuci novom Render API-u i nekim zaista radikalnim programskim ugradnjama (hook-ovima). Novi RDF modul omogucava oznacavanje sadrzaja namijenjeno Semantickom Webu.',
+  'Upravljajte točno s onim što i gdje će biti prikazano na ekranu zahvaljujući novom Render API-u i nekim zaista radikalnim programskim ugradnjama (hook-ovima). Novi RDF modul omogućava označavanje sadržaja namijenjeno Semantičkom Webu.',
   'Accessible' =>
-  'Pristupacnost',
+  'Pristupačnost',
   'Administration screens are now far more accessible. Many front-end improvements make it easier for you to build highly accessible websites.' =>
-  'Administracijsko sucelje sada je jos pristupacnije. Brojna poboljsanja sucelja omogucavaju laksu izradu dostupnijih web sjedista.',
+  'Administracijsko sučelje sada je još pristupačnije. Brojna poboljšanja sučelja omogućavaju lakšu izradu dostupnijih web sjedišta.',
   'Images and files' =>
   'Slike i datoteke',
   'Adding images to content is now built in. Generate different versions for thumbnails, previews and other image styles. Private file handling can now be used alongside public files.' =>
-  'Dodavanje slika sadrzaju sada je ugradeno. Automatski proizvedite razlicite velicine i stilove slika. Mozete kombinirati privatnu i javnu dostupnost datoteka.',
+  'Dodavanje slika sadržaju sada je ugrađeno. Automatski proizvedite različite veličine i stilove slika. Možete kombinirati privatnu i javnu dostupnost datoteka.',
   'Automated code testing' =>
   'Automatizirano testiranje koda',
   'A new automated testing framework with over 30,000 built-in tests allows for <a href="@qa-url">continuous integration testing</a> of all Drupal core patches and contributed modules.' =>
-  'Novo okruzje za automatsko testiranje koda s preko 30000 ugradenih testova omogucava <a href="@qa-url">neprestanu procjenu kvalitete koda</a> svih zakrpa Drupal jezgre i dodatnih modula.',
+  'Novo okružje za automatsko testiranje koda s preko 30000 ugrađenih testova omogućava <a href="@qa-url">neprestanu procjenu kvalitete koda</a> svih zakrpa Drupal jezgre i dodatnih modula.',
   'Improved database support' =>
-  'Poboljsana podrska rada s bazama podataka',
+  'Poboljšana podrška rada s bazama podataka',
   'A new database layer provides out-of-the-box support for SQLite, MySQL/MariaDB, and PostgreSQL. Install contributed modules to use <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a>, and more.' =>
-  'Novi sloj baze podataka pruza podrsku za SQLite, MySQL/MariaDB i PostgreSQL. Dodatni moduli omogucuju podrsku za <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a> i dr.',
+  'Novi sloj baze podataka pruža podršku za SQLite, MySQL/MariaDB i PostgreSQL. Dodatni moduli omogućuju podršku za <a href="@sqlsrv-url">MS SQL Server</a>, <a href="@oracle-url">Oracle</a> i dr.',
   'Better distribution support' =>
-  'Bolja podrska distribucijama',
+  'Bolja podrška distribucijama',
   'Use installation profiles to distribute your custom Drupal product. A new API and exportable configurations let you capture more settings in code.' =>
-  'Upotrijebite instalacijske profile kako biste distribuirali Vas prilagodeni Drupal. Novi API i mogucnost izvoza postavki omogucuju pohranjivanje jos vise postavki u kod.',
+  'Upotrijebite instalacijske profile kako biste distribuirali Vaš prilagođeni Drupal. Novi API i mogućnost izvoza postavki omogućuju pohranjivanje još više postavki u kod.',
   'Extend' =>
-  'Prosirenja',
+  'Proširenja',
   'Thanks to a great community effort, over 800 modules are available or under active development for Drupal 7, including <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a>, and <a href="@wysiwyg-url">WYSIWYG</a>, with more on the way every day.' =>
-  'Zahvaljujuci velikom trudu zajednice, dostupno je ili je u fazi razvoja preko 800 modula za Drupal 7, ukljucujuci <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a> i <a href="@wysiwyg-url">WYSIWYG</a>, a svakim danom ih je sve vise.',
+  'Zahvaljujući velikom trudu zajednice, dostupno je ili je u fazi razvoja preko 800 modula za Drupal 7, uključujući <a href="@views-url">Views</a>, <a href="@pathauto-url">Pathauto</a> i <a href="@wysiwyg-url">WYSIWYG</a>, a svakim danom ih je sve više.',
   'See Drupal 7 in action' =>
   'Pogledajte Drupal 7 u akciji',
   'Drupal 7 is already in use on hundreds of sites. Some examples include:' =>
-  'Stotine web sjedista vec koriste Drupal 7. Neki od primjera su:',
+  'Stotine web sjedišta već koriste Drupal 7. Neki od primjera su:',
   '<a href="@examiner-url">Examiner.com</a>' =>
   '<a href="@examiner-url">Examiner.com</a>',
   '<a href="@gardens-url">Drupal Gardens</a>' =>
@@ -95,15 +95,16 @@ $strings = array(
   'To install Drupal 7, you will need:' =>
   'Za instalaciju Drupala 7 potrebno je:',
   '<strong>Web Server</strong>: Apache (recommended), Nginx, Lighttpd, or Microsoft IIS' =>
-  '<strong>Web Server</strong>: Apache (preporucen), Nginx, Lighttpd ili Microsoft IIS',
+  '<strong>Web Server</strong>: Apache (preporučen), Nginx, Lighttpd ili Microsoft IIS',
   '<strong>Database</strong>: MySQL 5.0.15 and higher, PostgreSQL 8.3 and higher, or SQLite 3.x' =>
   '<strong>Baza podataka</strong>: MySQL 5.0.15 (ili noviji), PostgreSQL 8.3 (ili noviji) ili SQLite 3.x',
   '<strong>PHP</strong>: 5.2.4 and higher' =>
   '<strong>PHP</strong>: 5.2.4 (ili noviji)',
   '<strong>Memory</strong>: 32MB (A site with a number of commonly used modules enabled may require 64 MB of memory or more.)' =>
-  '<strong>Memorija</strong>: 32 MB (Web sjediste s vecim brojem modula moze zahtjevati 64 MB memorije ili vise.)',
+  '<strong>Memorija</strong>: 32 MB (Web sjedište s većim brojem modula može zahtjevati 64 MB memorije ili više.)',
   'More <a href="@requirements-url">requirements information</a>.' =>
-  'Vise <a href="@requirements-url">informacija o zahtjevima</a>.',
-  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7.' =>
-  'Nazocite <a href="@drupalcon-url">DrupalCon-u u Chicagu</a> kako biste naucili vise o Drupalu 7.',
+  'Više <a href="@requirements-url">informacija o zahtjevima</a>.',
+  'Attend <a href="@drupalcon-url">DrupalCon Chicago</a> to learn more about Drupal 7' =>
+  'Nazočite <a href="@drupalcon-url">DrupalCon-u u Chicagu</a> kako biste naučili više o Drupalu 7',
 );
+
