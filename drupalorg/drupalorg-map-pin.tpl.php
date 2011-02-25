fbddff3bbf56aa384ebb6af7c7bcd8ed18024a19
Neil Drumm <drumm@delocalizedham.com>
2011-02-25 14:48:44 -0800

    Make map pin rendering themeable, for reuse.

---
+++ drupalorg/drupalorg-map-pin.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file HTML to plot a map pin. JS does the positioning.
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <span class="latitude"><?php print $latitude; ?></span>
+  <span class="longitude"><?php print $longitude; ?></span>
+  <div class="content"><?php print $content; ?></div>
+</div>
