d3223d46054fbd0b35f68ec0156ec707d8a6ae2a
Neil Drumm <drumm@delocalizedham.com>
2011-04-26 21:12:58 -0700

    #1080574 initial page, a straight copy of http://association.drupal.org/node/78 with HTML cleanup.

---
+++ association_drupalorg/association-drupalorg-membership-page.tpl.php
@@ -0,0 +1,50 @@
+<p>To join the Drupal Association, you must first be signed in to Drupal.org</p>
+
+<p>Purchase or Renew a Drupal Association Membership:</p>
+<ul>
+  <li><a href="/civicrm/contribute/transact?reset=1&id=1">Buy an Individual Membership</a> for €22, approx $30.</li>
+  <li><a href="/civicrm/contribute/transact?reset=1&id=2">Buy an Organization Membership</a> for €73, approx $101.</li>
+</ul>
+
+<h2>Membership Benefits</h2>
+
+<h3>1. Membership Directory Listing</h3>
+<p>Members are listed in the public membership directories. Members who join or renew are listed in the Drupal Association's annual report.</p>
+<p><a href= "/civicrm/profile?reset=1&force=1&gid=8&search=0"> Directory of Individual Memberships</a></p>
+<p><a href= "/civicrm/profile?reset=1&force=1&gid=4&search=0"> Directory of Organization Memberships</a></p>
+
+<h3>2. Membership Badges</h3>
+<p>Members are granted free use of the Drupal Association Membership Badges while they remain active members. Members can put it on their websites, blogs, etc. A badge will also show in their Drupal.org user profile.</p>
+
+<h3>3. Drupal Marketplace</h3>
+<p>Organization Members who have contributed to Drupal can be listed in the <a href="http://drupal.org/marketplace-preview" alt="Drupal Marketplace">Drupal Marketplace</a>.</p>
+
+<h2>Where does it go?</h2>
+<p>The Drupal association can not take credit for the amazing success of the Drupal project, but will highlight the success of the community in it's efforts to keep supporting the project. Your membership makes the Drupal.org infrastructure possible and supports:</p>
+
+<h3>Community Initiatives:</h3>
+<ul>
+  <li><a href="http://groups.drupal.org/drupalorg-redesign-plan-drupal-association" alt="The Drupal.org Redesign">The Drupal.org Redesign</a></li>
+  <li><a href="http://drupal.org/community-initiatives/git">Drupal.org's Migration to Git</a></li>
+  <li>Upgrading the Drupal.org Infrastructure</li>
+  <li><a href="http://drupalcon.org" alt="DrupalCons">DrupalCons</a> and Drupal Camps in cities around the world</li>
+</ul>
+
+<h3>Community Members:</h3>
+<ul>
+  <li>550 000+ users on Drupal.org, 300 new users a day.</li>
+  <li>1600 users listed as service providers.</li>
+  <li>375 000 discussion threads: forums, mailing lists, groups, etc.</li>
+</ul>
+
+<h3>Open-source Development:</h3>
+<ul>
+  <li>Over 8600+ Drupal modules and themes available for download.</li>
+  <li>8800 unique individuals participating in technical issues and 1800 unique contributors posting code patches in the last 12 months.</li>
+  <li>44 000 issues filed with 160 000 follow-ups, a 1:4 response rate in the last 12 months.</li>
+</ul>
+
+<h2>What else can I do?</h2>
+<p>If you would like to give the association more than the membership fee you can also make a donation, offer to be a sponsor for Drupalcon, or sponsor a local Drupal event.  If you choose not to purchase an individual or organizational membership  you can support the Drupal project by <a href="/donate">donating to the Drupal Association</a>.</p>
+
+<p>If you have any questions or issues with memberships or donations, look in the <a href="/about/donations">donations FAQ</a> or <a href="/contact">Contact us</a>.</p>
