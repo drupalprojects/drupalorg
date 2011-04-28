05f802b50e3bb7e6cd7e4a2998e3263c6aea097b
Neil Drumm <drumm@delocalizedham.com>
2011-04-28 15:31:05 -0700

    #1080574 direct users to log in when buying. And a lot of copy changes.

--- association_drupalorg/association-drupalorg-membership-page.tpl.php
+++ association_drupalorg/association-drupalorg-membership-page.tpl.php
@@ -1,50 +1,55 @@
-<p>To join the Drupal Association, you must first be signed in to Drupal.org</p>
+<h2>Join the Drupal Association</h2>
 
-<p>Purchase or Renew a Drupal Association Membership:</p>
-<ul>
-  <li><a href="/civicrm/contribute/transact?reset=1&id=1">Buy an Individual Membership</a> for €22, approx $30.</li>
-  <li><a href="/civicrm/contribute/transact?reset=1&id=2">Buy an Organization Membership</a> for €73, approx $101.</li>
-</ul>
+<div class="grid-8 alpha">
+  <p>Purchase or renew a Drupal Association membership:</p>
+  <ul>
+    <li><a href="<?php print $individual_url; ?>">Buy an individual membership</a> for €22, approx $33</li>
+    <li><a href="<?php print $organization_url ?>">Buy an organization membership</a> for €73, approx $108</li>
+  </ul>
 
-<h2>Membership Benefits</h2>
+  <h3>Membership benefits</h3>
 
-<h3>1. Membership Directory Listing</h3>
-<p>Members are listed in the public membership directories. Members who join or renew are listed in the Drupal Association's annual report.</p>
-<p><a href= "/civicrm/profile?reset=1&force=1&gid=8&search=0"> Directory of Individual Memberships</a></p>
-<p><a href= "/civicrm/profile?reset=1&force=1&gid=4&search=0"> Directory of Organization Memberships</a></p>
+  <h4>1. Membership directory listing</h4>
+  <p>Members are listed in the public membership directories. Members who join or renew are listed in the Drupal Association's annual report.</p>
+  <ul>
+    <li><a href= "/civicrm/profile?reset=1&force=1&gid=8&search=0">Directory of individual memberships</a></li>
+    <li><a href= "/civicrm/profile?reset=1&force=1&gid=4&search=0">Directory of organization memberships</a></li>
+  </ul>
 
-<h3>2. Membership Badges</h3>
-<p>Members are granted free use of the Drupal Association Membership Badges while they remain active members. Members can put it on their websites, blogs, etc. A badge will also show in their Drupal.org user profile.</p>
+  <h4>2. Membership badges</h4>
+  <p>Members are granted free use of the Drupal Association membership badges while they remain active members. Members can put it on their websites, blogs, etc. A badge will also show in their Drupal.org user profile.</p>
 
-<h3>3. Drupal Marketplace</h3>
-<p>Organization Members who have contributed to Drupal can be listed in the <a href="http://drupal.org/marketplace-preview" alt="Drupal Marketplace">Drupal Marketplace</a>.</p>
+  <h4>3. Drupal marketplace</h4>
+  <p>Organization members who have contributed to Drupal can be listed in the <a href="http://drupal.org/marketplace-preview">Drupal marketplace</a>.</p>
 
-<h2>Where does it go?</h2>
-<p>The Drupal association can not take credit for the amazing success of the Drupal project, but will highlight the success of the community in it's efforts to keep supporting the project. Your membership makes the Drupal.org infrastructure possible and supports:</p>
+  <h3>Where does it go?</h3>
+  <p>The Drupal association can not take credit for the amazing success of the Drupal project, but will highlight the success of the community in it's efforts to keep supporting the project. Your membership makes the Drupal.org infrastructure possible and supports:</p>
 
-<h3>Community Initiatives:</h3>
-<ul>
-  <li><a href="http://groups.drupal.org/drupalorg-redesign-plan-drupal-association" alt="The Drupal.org Redesign">The Drupal.org Redesign</a></li>
-  <li><a href="http://drupal.org/community-initiatives/git">Drupal.org's Migration to Git</a></li>
-  <li>Upgrading the Drupal.org Infrastructure</li>
-  <li><a href="http://drupalcon.org" alt="DrupalCons">DrupalCons</a> and Drupal Camps in cities around the world</li>
-</ul>
+  <h4>Community initiatives:</h4>
+  <ul>
+    <li><a href="http://groups.drupal.org/drupalorg-redesign-plan-drupal-association">Drupal.org redesign</a></li>
+    <li><a href="http://drupal.org/community-initiatives/git">Drupal.org's migration to Git</a></li>
+    <li>Upgrading the Drupal.org infrastructure</li>
+    <li><a href="http://drupalcon.org" alt="DrupalCons">DrupalCons</a> and Drupal camps in cities around the world</li>
+  </ul>
+</div>
 
-<h3>Community Members:</h3>
-<ul>
-  <li>550 000+ users on Drupal.org, 300 new users a day.</li>
-  <li>1600 users listed as service providers.</li>
-  <li>375 000 discussion threads: forums, mailing lists, groups, etc.</li>
-</ul>
+<div class="grid-4 omega">
+  <?php /* todo: Your membership status */ ?>
 
-<h3>Open-source Development:</h3>
-<ul>
-  <li>Over 8600+ Drupal modules and themes available for download.</li>
-  <li>8800 unique individuals participating in technical issues and 1800 unique contributors posting code patches in the last 12 months.</li>
-  <li>44 000 issues filed with 160 000 follow-ups, a 1:4 response rate in the last 12 months.</li>
-</ul>
+  <?php /* todo: Want to give more? */ ?>
 
-<h2>What else can I do?</h2>
-<p>If you would like to give the association more than the membership fee you can also make a donation, offer to be a sponsor for Drupalcon, or sponsor a local Drupal event.  If you choose not to purchase an individual or organizational membership  you can support the Drupal project by <a href="/donate">donating to the Drupal Association</a>.</p>
+  <h3 class="top">Association support in numbers</h3>
+  <?php /* todo: Automate */ ?>
+  <ul>
+    <li><em>2,881 attendees</em> at <a href="http://chicago2011.drupal.org">DrupalCon Chicago</a></li>
+    <li><a href="http://drupal.org/">Drupal.org</a> server infrastructure for <em>578,400+ people</em> and <em>9,384+ modules</em></li>
+    <li>Migration to Git to support <em>thousands of commits per week</em></li>
+  </ul>
 
-<p>If you have any questions or issues with memberships or donations, look in the <a href="/about/donations">donations FAQ</a> or <a href="/contact">Contact us</a>.</p>
+  <h3>Have any questions?</h3>
+  <ul class="flat">
+    <li><a href="/about/faq">Freqently asked questions</a></li>
+    <li><a href="/contact">Contact us</a></li>
+  </ul>
+</div>
