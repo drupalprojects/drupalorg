3afa78e99d0598e005a7d8f4ddbd1b896e074959
Neil Drumm <drumm@delocalizedham.com>
2011-05-25 18:36:31 -0700

    #1169138 Require login for Association donation.

---
+++ association_drupalorg/association-drupalorg-donate-page.tpl.php
@@ -0,0 +1,22 @@
+<p>You must be logged in to donate to the Drupal Association so we can thank you in our <a href="http://association.drupal.org/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>.</p>
+
+<p><a href="<?php print $donate_url; ?>" class="action-button">Donate to the Drupal Association</a></p>
+
+The Drupal Association also accepts donations through PayPal or by direct deposit into our bank account. You can also become an <a href="http://association.drupal.org/civicrm/contribute/transact?reset=1&id=1">Individual Member (€22)</a>  or an <a href="http://association.drupal.org/civicrm/contribute/transact?reset=1&id=2">Organization Member (€73)</a> of the Drupal Association.
+
+<h3>Bank account</h3>
+
+<p>To deposit your donation directly into our Belgian bank account, you can use the information below. Please mention your name or company name, and your e-mail address in the subject/communication of your payment. This information will be used to manually add your donation to the donor list. Our bank account accepts all currencies. Donations by direct bank deposit should not necessarily be done in euro; you can donate using your own currency.</p>
+
+<p>Account holder: Drupal vzw</p>
+
+<p>Bank:<br />
+KBC Bank NV<br />
+Havenlaan 2<br />
+1080 Brussel<br />
+Belgium</p>
+
+<p>Account number:<br />
+IBAN: BE57 7330 3960 5635<br />
+BIC: KREDBEBB<br />
+733-0396056-35</p>
