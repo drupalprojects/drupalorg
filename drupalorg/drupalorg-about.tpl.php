57abf2ea85056218751cb2c9acb78adf4d3b949a
Lisa Rex <lisa@lisam.rex@gmail.com>
2011-07-26 17:20:02 -0400

    Issue #947176 by dww, lisarex: added links and improvements to About page.

--- drupalorg/drupalorg-about.tpl.php
+++ drupalorg/drupalorg-about.tpl.php
@@ -1,75 +1,5 @@
 <p>Drupal is a free software package that allows anyone to easily publish, manage and organize a wide variety of content on a website. Hundreds of thousands of people and organizations are using Drupal to power an endless variety of sites.</p>
 
-<div class="node-section grid-12 alpha omega" id="about-features">
-  <h2>Drupal Content Management Features</h2>
-  
-  <div class="node-sub-section grid-12 alpha omega">
-    <div class="grid-3 alpha">
-      <h3 id="aboutpersonalisation"><a href="/features/build">BUILD</a></h3>
-      <p>Build internal and external-facing websites in a matter of hours, with no custom programming. No need to start from scratch every time you build a new site, either. You can utilize a multi-site configuration or choose a Drupal distribution (installation profile). These provide a pre-defined configuration of site features and functions for a specific purpose or type of site, available as a single download.</p>
-    </div>
-      
-    <div class="grid-3">
-      <h3 id="aboutwebbased"><a href="/features/create">CREATE</a></h3>      
-      <p>Manage content with an easy-to-use web interface. Drupal's flexibility handles countless content types including video, text, blog, podcasts, and polls with robust user management, menu handling, real-time statistics and optional revision control.</p>
-    </div>
-      
-    <div class="grid-3">
-      <h3 id="aboutsearching"><a href="/features/administer">ADMINISTER</a></h3>
-      <p>Drupal comes with great options for new user accounts and user permissions. Users can be assigned one or more roles, and each role can be set up with fine-grained permissions allowing users view and create only what the administrator permits. The Drupal 7 admin theme makes administering a site easier than ever.</p>
-    </div>
-      
-    <div class="grid-3 omega">
-      <h3 id="aboutthemes"><a href="/features/design">DESIGN AND DISPLAY</a></h3>
-      <p>Drupal's presentation layer allows designers to create highly usable, interactive experiences that engage users and increase traffic. Use an existing Drupal theme for your site, giving it an instant identity! If you can't find one you like, try designing your own which others can use, too.</p>
-    </div>
-  </div>
-  
-  <div class="node-sub-section grid-12 alpha omega">
-    <div class="grid-3 alpha">
-      <h3 id="aboutcontentmanagement"><a href="/features/organize">ORGANIZE & FIND</a></h3>
-      <p>Drupal comes with many tools to help you organize, structure, find and re-use your content. Categorize with taxonomy, automatically create friendly path urls, create custom lists, associate content with other content on your site, and create smart defaults for content creators.</p>
-    </div>
-      
-    <div class="grid-3">
-      <h3 id="aboutmodules"><a href="/features/extend">EXTEND</a></h3>
-      <p>With more than 6000 available modules, the vast majority of your site's requirements can be addressed with Drupal core and available add-on modules. Focus your efforts on building great web sites, not re-creating features available elsewhere. You can even <a href="/contribute">contribute modules</a> for others to use.</p>
-    </div>
-      
-    <div class="grid-3">
-      <h3 id="aboutcommunity"><a href="/features/collaborate">COLLABORATE</a></h3>
-      <p>Drupal's focus on social publishing can help you create sites and applications to help your users express their opinions and engage with one another. You can have tight control over who can create, view, administer, publish and otherwise interact with content on your site.</p>
-    </div>
-      
-    <div class="grid-3 omega">
-      <h3 id="aboutsocial"><a href="/features/connect">CONNECT</a></h3>
-      <p>Drupal makes it easy to connect your website to other sites and services across the web, using aggregation, feeds, and search engine connection capabilities. Social networking integration is also widely supported to help you engage with a wider audience. Drupal also offers your site easy ways to interact with external media and file services.</p>
-    </div>
-  </div>
-</div>
-
-<div class="node-section grid-12 alpha omega">
-  <div class="grid-3 alpha">
-    <h2>Drupal is Open Source</h2>
-    <p>Drupal is open source software maintained and developed by a community of hundreds of thousands of users and developers. It's distributed under the terms of the <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a> (or "GPL"), which means anyone is free to download it, share it with others, and <a href="http://drupal.org/contribute">contribute</a> back to the project.  This open development model means that people are constantly working to make sure Drupal is a cutting-edge platform that supports the latest technologies that the Web has to offer. </p>
-  </div>
-      
-  <div class="grid-3">
-    <h2>Community Support</h2>
-    <p>The passionate <a href="/community">Drupal community</a> is on hand to give support via various <a href="/irc">vibrant IRC channels</a>, in the <a href="/forum">forums</a>, and face to face at <a href="http://groups.drupal.org/events">Drupal events</a>. The community has also created <a href="/documentation">Documentation</a> for Drupal, which covers installation, site building, module building, theming, and many other topics. The worldwide community drives the innovation that makes Drupal the preferred choice for web developers and site owners. Drupal is <a href="/principles">open source</a>, meaning everybody can <a href="/getting-involved">get involved</a> and make a difference.</p>
-  </div>
-      
-  <div class="grid-3">
-    <h2>Security Team</h2>
-    <p><a href="/documentation/is-drupal-secure">We take security seriously</a> and have a <a href="/security-team">dedicated team</a> that has a <a href="/about/security-track-record">proven track record</a> in dealing with your security issues professionally and efficiently, meaning you can spend more time creating your site.</p>
-  </div>
-      
-  <div class="grid-3 omega">
-    <h2>Commercial Services</h2>
-    <p>As well as the community, there are <a href="/services">many dedicated companies</a> in the Marketplace on hand to help with your Drupal project. Providing expertise and a deeper understanding, they can help with <a href="/hosting">hosting</a>, spam blocking, <a href="/training-services">training</a>, and more.</p>
-  </div>
-</div>
-
 <div class="node-footer-section grid-12 alpha omega">
   <div class="grid-4 alpha">
     <h3>Who's Using Drupal</h3>
@@ -103,7 +33,7 @@
       <p>Read more about how to build, administer and design with <a href="/features">Drupal's Content Management Features</a>.</p>
       
       <h3>Getting Started</h3>
-      <p>The Drupal Core is a collection of files that you must download in order to get started with Drupal. </p>
+      <p><a href="/start">Get Started with Drupal</a> gives an overview of working with Drupal, or you can check out a pre-built <a href="/project/installation+profiles">Drupal distribution</a> or <a href="/download">download Drupal core</a>. </p>
       <p><?php print $core_download_button; ?></p>
       <p class="clear">You can then build upon that with <a href="/project/modules">modules</a>, <a href="/project/themes">themes</a>, and <a href="http://localize.drupal.org">translations</a>.</p>
     </div>
@@ -121,5 +51,27 @@
       <p>Drupal is web-based, which means it is compatible with all operating systems.</p>
       <p>More detailed information on the <a href="/requirements">system requirements</a>.</p>
     </div>
+  </div>
+</div>
+
+<div class="node-section grid-12 alpha omega">
+  <div class="grid-3 alpha">
+    <h2>Drupal is Open Source</h2>
+    <p>Drupal is open source software maintained and developed by a community of hundreds of thousands of users and developers. It's distributed under the terms of the <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a> (or "GPL"), which means anyone is free to download it, share it with others, and <a href="http://drupal.org/contribute">contribute</a> back to the project.  This open development model means that people are constantly working to make sure Drupal is a cutting-edge platform that supports the latest technologies that the Web has to offer. </p>
+  </div>
+      
+  <div class="grid-3">
+    <h2>Community Support</h2>
+    <p>The passionate <a href="/community">Drupal community</a> is on hand to give support via various <a href="/irc">vibrant IRC channels</a>, in the <a href="/forum">forums</a>, and face to face at <a href="http://groups.drupal.org/events">Drupal events</a>. The community has also created <a href="/documentation">Documentation</a> for Drupal, which covers installation, site building, module building, theming, and many other topics. The worldwide community drives the innovation that makes Drupal the preferred choice for web developers and site owners. Drupal is <a href="/principles">open source</a>, meaning everybody can <a href="/getting-involved">get involved</a> and make a difference.</p>
+  </div>
+      
+  <div class="grid-3">
+    <h2>Security Team</h2>
+    <p><a href="/documentation/is-drupal-secure">We take security seriously</a> and have a <a href="/security-team">dedicated team</a> that has a <a href="/about/security-track-record">proven track record</a> in dealing with your security issues professionally and efficiently, meaning you can spend more time creating your site.</p>
+  </div>
+      
+  <div class="grid-3 omega">
+    <h2>Commercial Services</h2>
+    <p>As well as the community, there are <a href="/services">many dedicated companies</a> in the Marketplace on hand to help with your Drupal project. Providing expertise and a deeper understanding, they can help with <a href="/hosting">hosting</a>, spam blocking, <a href="/training-services">training</a>, and more.</p>
   </div>
 </div>
