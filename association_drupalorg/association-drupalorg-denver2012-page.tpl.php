855642d4c4a8018f732a10f025218af6aa4069e5
Neil Drumm <drumm@delocalizedham.com>
2011-08-14 19:34:18 -0700

    Clean up Denver 2012 registration's user profile information.

--- association_drupalorg/association-drupalorg-denver2012-page.tpl.php
+++ association_drupalorg/association-drupalorg-denver2012-page.tpl.php
@@ -8,13 +8,15 @@
   <a href="http://maps.google.com/maps/place?q=colorado+convention+center&cid=11112440205075771742">Colorado Convention Center</a></p>
 </div>
 
-<h2>Personal Information</h2>
-<p>We use your Drupal.org account.</p>
+<h2>Personal information</h2>
+<p>We use your Drupal.org account. This info can be updated on Drupal.org at any time, and may be printed on your badge.</p>
 
 <?php if (user_is_logged_in()) { ?>
   <div class="profile"><dl class="clear-block"><?php print $personal_info; ?></dl></div>
+  <?php print $personal_info_edit; ?>
   <h3>Work</h3>
   <div class="profile"><dl class="clear-block"><?php print $work_info; ?></dl></div>
+  <?php print $work_info_edit; ?>
 <?php } else { // Not logged in. ?>
   <p>Please <a href="<?php print url('user', array('query' => drupal_get_destination())); ?>">log in</a>.
 <?php } ?>
