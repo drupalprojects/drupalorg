1e39e43f81d4d3440d78b9b5a047ca263ac52193
Neil Drumm <drumm@delocalizedham.com>
2011-10-20 17:25:09 -0700

    Use regular blocks on membership page.

--- association_drupalorg/association-drupalorg-membership-page.tpl.php
+++ association_drupalorg/association-drupalorg-membership-page.tpl.php
@@ -1,74 +1,57 @@
-<div class="grid-8 alpha">
-  <h2>Become an Individual Member</h2>
-  <p><a href="<?php print $individual_usd_url; ?>" class="action-button">$30 for 1 year</a> or <a href="<?php print $individual_eur_url; ?>" class="action-button">€22 for 1 year</a></p>
+<h2>What is Drupal Association membership?</h2>
+<p>The Drupal Association can not take credit for the amazing success of the Drupal project, but will highlight the success of the community in its efforts to keep supporting the project. We are supported by Individuals and Organizations who become yearly members. Our membership makes the Drupal.org infrastructure possible and supports <a href="/projects">community initiatives</a>. Organization Membership information is below the Individual Memberships, scroll down to see.</p>
 
-  <h3>Why Join?</h3>
-  <p>Individual Members can show they’re an active part of the Drupal community. Memberships help pay for Drupal.org infrastructure and financing large community initiatives.</p>
+<h2>Become an Individual Member</h2>
+<p>Annual membership for individuals supports the Drupal Association and its <a href="/projects">community projects</a>.</p>
 
-  <h3>Benefits:</h3>
-  <ul>
-    <li><a href="/civicrm/profile?reset=1&force=1&gid=8&search=0&crmRowCount=10000">Directory of Individual Members</a> - Members are listed in our public directory of Individual Members</li>
-    <li><a href="https://association.drupal.org/membership/benefits">Discounts</a> - Members receive discounts from Drupal Association partners like Zend, Varnish Software, ActiveState and Acquia.</li>
-    <li>Promote - Members are listed in the <a href="https://association.drupal.org/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>, given out at major Drupal events and online</li>
-    <li><a href="http://drupal.org/image/tid/112">Membership Badge</a> - Let the world know you’re a part of the Drupal community on your Drupal.org profile and beyond</li>
-    <li>First Alert - Early notice of upcoming promotional programs and Association initiatives</li>
-  </ul>
+<h3>Individual Membership</h3>
+<p><a href="<?php print $individual_usd_url; ?>" class="action-button">$30 for 1 year</a> or <a href="<?php print $individual_eur_url; ?>" class="action-button">€22 for 1 year</a></p>
 
-  <h2>Become an Organization Member</h2>
-  <p><a href="<?php print $organization_usd_url; ?>" class="action-button">$100 for 1 year</a> or <a href="<?php print $organization_eur_url; ?>" class="action-button">€73 for 1 year</a></p>
+<h3>Benefits:</h3>
+<dl>
+  <dt><a href="/civicrm/profile?reset=1&force=1&gid=8&search=0&crmRowCount=10000">Directory of Individual Members</a></dt>
+  <dd>Members are listed in our public directory of Individual Members</dd>
+  <dt><a href="/membership/benefits">Discounts</a></dt>
+  <dd>Members receive discounts from Drupal Association partners like Zend, Varnish Software, ActiveState and Acquia.</dd>
+  <dt>Promote</dt>
+  <dd>Members are listed in the <a href="/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>, given out at major Drupal events and online</dd>
+  <dt><a href="http://drupal.org/image/tid/112">Membership Badge</a></dt>
+  <dd>Let the world know you’re a part of the Drupal community on your Drupal.org profile and beyond</dd>
+  <dt>First Alert</dt>
+  <dd>Early notice of upcoming promotional programs and Association initiatives</dd>
+</dl>
 
-  <h3>Why Join?</h3>
-  <p>Not only does your membership support the community, but you also receive unique promotional opportunities. And, by joining, you can tout the depth of your involvement to your prospective Drupal clients.</p>
+<h2>Membership for Organizations</h2>
+<p>Organization Membership is the annual membership for firms and organizations to support the Drupal Association and its <a href="/projects">community projects</a>. Not only does your membership support the community, but you also receive unique promotional opportunities. And, by joining, you can tout the depth of your involvement to your prospective Drupal clients.</p>
 
-  <h3>Benefits:</h3>
-  <ul>
-    <li><a href="http://drupal.org/marketplace-preview">Drupal Marketplace</a> - Organization Members who have contributed to Drupal can be listed in the Drupal Marketplace, which receives 23,000 monthly visitors</li>
-    <li>Advertising - Communicate contextual, relevant and value-add messaging tothe developer community with web banners on fixed, well approved areas
-    <li>Get Connected - Grow your business partnerships. We’ll make introductions at business networking events, so you can accelerate your business</li>
-    <li><a href="https://association.drupal.org/membership/benefits">Discounts</a> - Members receive discounts from Drupal Association partners like Zend, Varnish Software, ActiveState and Acquia.</li>
-    <li><a href="/civicrm/profile?reset=1&force=1&gid=4&search=0&crmRowCount=10000">Organization Directory Listing</a> - Members are listed in our public directory of Organization Members</li>
-    <li>Promote - Members are listed in the <a href="https://association.drupal.org/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>, given out at major Drupal events and online</li>
-    <li>Business-focused Newsletter - Provides updates on new promotional opportunities, Drupal Association news, and more!</li>
-    <li>First Alert - Early notice of upcoming promotional programs and Association initiatives</li>
-    <li><a href="http://drupal.org/image/tid/113">Membership Badge</a> - Let the world know you’re a part of the Drupal community on your Drupal.org profile and beyond</li>
-  </ul>
+<h3>Become an Organization Member</h3>
+<p><a href="<?php print $organization_usd_url; ?>" class="action-button">$100 for 1 year</a> or <a href="<?php print $organization_eur_url; ?>" class="action-button">€73 for 1 year</a></p>
 
-  <h2>What else can I do?</h2>
-  <p>If you would like to give more to the Association and the community, you can also make a donation, offer to be a sponsor for Drupalcon, or sponsor a local Drupal event.</p>
-  <p><a href="donate" class="action-button">Donate to the Drupal Association</a></p>
+<h3>Why Join?</h3>
+<p>Not only does your membership support the community, but you also receive unique promotional opportunities. And, by joining, you can tout the depth of your involvement to your prospective Drupal clients.</p>
 
-  <h2>Where does it go?</h2>
-  <p>The Drupal Association can not take credit for the amazing success of the Drupal project, but will highlight the success of the community in it’s efforts to keep supporting the project. Your membership makes the Drupal.org infrastructure possible and supports community initiatives:</p>
-  <ul>
-    <li><a href="http://groups.drupal.org/drupalorg-redesign-plan-drupal-association">Drupal.org redesign</a></li>
-    <li><a href="http://drupal.org/community-initiatives/git">Drupal.org’s migration to Git</a></li>
-    <li>Upgrading the Drupal.org infrastructure</li>
-    <li><a href="http://drupalcon.org/">DrupalCons</a> and Drupal camps in cities around the world</a></li>
-  </ul>
-</div>
+<h3>Benefits:</h3>
+<dl>
+  <dt><a href="http://drupal.org/marketplace-preview">Drupal Marketplace</a></dt>
+  <dd>Organization Members who have contributed to Drupal can be listed in the Drupal Marketplace, which receives 23,000 monthly visitors</dd>
+  <dt><a href="/advertising">Advertising</a></dt>
+  <dd>Communicate contextual, relevant and value-add messaging tothe developer community with web banners on fixed, well approved areas
+  <dt>Get Connected</dt>
+  <dd>Grow your business partnerships. We’ll make introductions at business networking events, so you can accelerate your business</dd>
+  <dt><a href="/membership/benefits">Discounts</a></dt>
+  <dd>Members receive discounts from Drupal Association partners like Zend, Varnish Software, ActiveState and Acquia.</dd>
+  <dt><a href="/civicrm/profile?reset=1&force=1&gid=4&search=0&crmRowCount=10000">Organization Directory Listing</a></dt>
+  <dd>Members are listed in our public directory of Organization Members</dd>
+  <dt>Promote</dt>
+  <dd>Members are listed in the <a href="/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>, given out at major Drupal events and online</dd>
+  <dt>Business-focused Newsletter</dt>
+  <dd>Provides updates on new promotional opportunities, Drupal Association news, and more!</dd>
+  <dt>First Alert</dt>
+  <dd>Early notice of upcoming promotional programs and Association initiatives</dd>
+  <dt><a href="http://drupal.org/image/tid/113">Membership Badge</a></dt>
+  <dd>Let the world know you’re a part of the Drupal community on your Drupal.org profile and beyond</dd>
+</dl>
 
-<div class="grid-4 omega">
-  <?php if (user_is_logged_in()) { ?>
-    <h3>Your membership status</h3>
-    <?php foreach ($memberships as $membership) { ?>
-      <p><?php print $membership; ?></p>
-    <?php } ?>
-  <?php } ?>
-
-  <h3>Current membership discounts</h3>
-  <p>The Drupal Association has partnered with companies like Zend, Varnish, ActiveState and Acquia to secure discounts at their stores. <a href="https://association.drupal.org/membership/benefits">See our partner discounts</a>.</p>
-
-  <h3>Association support in numbers</h3>
-  <?php /* todo: Automate */ ?>
-  <ul>
-    <li><em>2,881 attendees</em> at <a href="http://chicago2011.drupal.org">DrupalCon Chicago</a></li>
-    <li><a href="http://drupal.org/">Drupal.org</a> server infrastructure for <em>578,400+ people</em> and <em>9,384+ modules</em></li>
-    <li>Migration to Git to support <em>thousands of commits per week</em></li>
-  </ul>
-
-  <h3>Have any questions?</h3>
-  <ul class="flat">
-    <li><a href="/about/faq">Freqently asked questions</a></li>
-    <li><a href="/contact">Contact us</a></li>
-  </ul>
-</div>
+<h2>What else can I do?</h2>
+<p>If you would like to give more to the Association and the community, you can also make a donation, offer to be a sponsor for Drupalcon, or sponsor a local Drupal event.</p>
+<p><a href="donate" class="action-button">Donate to the Drupal Association</a></p>
