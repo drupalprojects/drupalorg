bb82c4b60f3864bd476237e889532dfd47c6f242
Neil Drumm <drumm@delocalizedham.com>
2011-10-27 12:29:30 -0700

    Make JS for hiding t-shirt with DMB a little better.

--- association_drupalorg/association_drupalorg.js
+++ association_drupalorg/association_drupalorg.js
@@ -1,14 +1,13 @@
 // Hide Shirt option for Drupal Means Business registrations.
 Drupal.behaviors.associationDrupalorg = function (context) {
   $(Drupal.settings.associationDrupalorgConfReg + ':not(.associationDrupalorg-processed)', context).each(function () {
-    $this = $(this).addClass('associationDrupalorg-processed')
-    $this.change(function () {
+    $(this).addClass('associationDrupalorg-processed').change(function () {
       if ($(this).val() == Drupal.settings.associationDrupalorgDMB) {
         $(Drupal.settings.associationDrupalorgShirt).slideUp('fast');
       }
       else {
         $(Drupal.settings.associationDrupalorgShirt).slideDown('fast');
       }
-    });
+    }).filter(':checked').change();
   });
 };
