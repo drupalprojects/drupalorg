e13f757e8b044a69447c52dd1b07ff808333f2fd
Neil Drumm <drumm@delocalizedham.com>
2011-11-03 15:45:05 -0700

    Add more sections to the DrupalCon report.

--- association_drupalorg/association-drupalorg-drupalcon-report-page.tpl.php
+++ association_drupalorg/association-drupalorg-drupalcon-report-page.tpl.php
@@ -1,8 +1,13 @@
 <p>
   <?php print $time_left; ?><br />
-  <?php print $total_registration; ?>
+  <?php print $download; ?>
 </p>
 
-<h2>Coupons</h2>
+<h2>Registrations</h2>
+<?php print $registration_sums; ?>
 
+<h2>Revenue</h2>
+<?php print $payment_sums; ?>
+
+<h2>Coupons</h2>
 <?php print $coupons; ?>
