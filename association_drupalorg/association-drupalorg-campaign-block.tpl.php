5cf1bd04d9134190a0224e1ba1f5f219072f23ec
Neil Drumm <drumm@delocalizedham.com>
2011-11-08 16:41:32 -0800

    Add a block for an Association campaign.

---
+++ association_drupalorg/association-drupalorg-campaign-block.tpl.php
@@ -0,0 +1,3 @@
+<div class="giant"><?php print $amount; ?></div>
+
+<p>Proceeds raised from our Membership + T-shirt sales are funding Drupal.org improvements until January 1, 2012. <a href="<?php print $learn_more_url; ?>">Learn more.</a></p>
