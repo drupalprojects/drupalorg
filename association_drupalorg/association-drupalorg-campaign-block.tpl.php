9daf6d5d9d82d7eefd882542265dc656120764a6
Neil Drumm <drumm@delocalizedham.com>
2011-11-14 19:05:47 -0800

    Now we are lengthening the campaign.

--- association_drupalorg/association-drupalorg-campaign-block.tpl.php
+++ association_drupalorg/association-drupalorg-campaign-block.tpl.php
@@ -1,3 +1,3 @@
 <div class="giant"><?php print $amount; ?></div>
 
-<p>Proceeds raised from our Membership + T-shirt sales are funding Drupal.org improvements until January 1, 2012. <a href="<?php print $learn_more_url; ?>">Learn more.</a></p>
+<p>Proceeds raised from our Membership + T-shirt sales are funding Drupal.org improvements until <a href="http://denver2012.drupal.org/">DrupalCon Denver</a>. <a href="<?php print $learn_more_url; ?>">Learn more.</a></p>
