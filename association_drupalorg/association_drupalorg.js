dbf93ef3c59a1536cb65cf5f0cad28c512cbcd36
Neil Drumm <drumm@delocalizedham.com>
2011-11-28 19:53:46 -0800

    Add DrupalCon training.

--- association_drupalorg/association_drupalorg.js
+++ association_drupalorg/association_drupalorg.js
@@ -1,12 +1,15 @@
 // Hide Shirt option for Drupal Means Business registrations.
 Drupal.behaviors.associationDrupalorg = function (context) {
   $(Drupal.settings.associationDrupalorgConfReg + ':not(.associationDrupalorg-processed)', context).each(function () {
+    $(Drupal.settings.associationDrupalorgShirt).hide();
     $(this).addClass('associationDrupalorg-processed').change(function () {
-      if ($(this).val() == Drupal.settings.associationDrupalorgDMB) {
-        $(Drupal.settings.associationDrupalorgShirt).slideUp('fast');
-      }
-      else {
-        $(Drupal.settings.associationDrupalorgShirt).slideDown('fast');
+      if (Drupal.settings.associationDrupalorgHasShirt.indexOf(parseInt($(this).val(), 10)) !== -1) {
+        if ($(this).attr('checked')) {
+          $(Drupal.settings.associationDrupalorgShirt).slideDown('fast');
+        }
+        else {
+          $(Drupal.settings.associationDrupalorgShirt).slideUp('fast');
+        }
       }
     }).filter(':checked').change();
   });
