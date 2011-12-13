96ee55be4d89677e13e920fe8cdba3bbe4684764
Neil Drumm <drumm@delocalizedham.com>
2011-12-12 17:44:14 -0800

    #1366670 Move all association-related code to a separate project + Git repo at http://drupal.org/project/association_drupalorg.

--- association_drupalorg/association-drupalorg-campaign-block.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="giant"><?php print $amount; ?></div>
-
-<p>Proceeds raised from our Membership + T-shirt sales are funding Drupal.org improvements until <a href="http://denver2012.drupal.org/">DrupalCon Denver</a>. <a href="<?php print $learn_more_url; ?>">Learn more.</a></p>
--- association_drupalorg/association-drupalorg-donate-page.tpl.php
+++
@@ -1,5 +0,0 @@
-<p>You must be logged in to donate to the Drupal Association so we can thank you in our <a href="https://association.drupal.org/system/files/Annual%20Report%202011%20-%20web%20short%20compressed.pdf">Drupal Association Annual Report</a>.</p>
-
-<p><a href="<?php print $donate_usd_url; ?>" class="action-button">Donate in U.S. Dollars</a> or <a href="<?php print $donate_eur_url; ?>" class="action-button">Donate in Euro</a></p>
-
-You can also become an <a href="https://association.drupal.org/civicrm/contribute/transact?reset=1&id=1">Individual Member (€22)</a>  or an <a href="https://association.drupal.org/civicrm/contribute/transact?reset=1&id=2">Organization Member (€73)</a> of the Drupal Association.
