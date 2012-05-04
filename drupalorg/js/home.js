2ba841c53e2eff2af8fb5b6d001fd06d320d4457
Neil Drumm <drumm@delocalizedham.com>
2012-05-04 20:52:41 +0000

    [#1544984] Make the home page map work.

--- drupalorg/js/home.js
+++ drupalorg/js/home.js
@@ -1,12 +1,12 @@
-$(function () {
+(function ($) {
   var current = 0,
     $map = $('.homepage-map'),
     $pins = $('.homepage-map>.homepage-pin');
 
   $pins.each(function () {
     $(this).css({
-      left: '' + Drupal.html5UserGeolocationLongitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
-      bottom: '' + Drupal.html5UserGeolocationLatitudeToPx($('>.latitude', this).text(), 78, -58, $map.height()) + 'px'
+      left: '' + Drupal.longitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
+      bottom: '' + Drupal.latitudeToPx($('>.latitude', this).text(), 78, -58, $map.height()) + 'px'
     });
   }).eq(current).show();
   setInterval(function () {
@@ -17,8 +17,6 @@ $(function () {
     }
     $pins.eq(current).show();
   }, 4000);
-});
 
-Drupal.behaviors.frontTabs = function () {
   $('#rotate > ul').tabs();
-};
+})(jQuery);
