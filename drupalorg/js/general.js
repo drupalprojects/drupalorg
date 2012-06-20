99c3310e6382ba3fc774cf102a5aaae4590034a5
Neil Drumm <drumm@delocalizedham.com>
2012-06-20 00:28:15 +0000

    [#1599064] Fix up dashboard for D7, make this my home page link

--- drupalorg/js/general.js
+++ drupalorg/js/general.js
@@ -1,16 +1,3 @@
-Drupal.behaviors.drupalorgSetHome = function () {
-  $('#drupalorg-set-home:not(.drupalorg-set-home-processed)')
-  .addClass('drupalorg-set-home-processed')
-  .each(function () {
-    var $this = $(this);
-    // Click triggers form submit
-    $('a', $this).click(function () {
-      $('input[type=submit]', $this).click();
-      return false;
-    });
-  });
-};
-
 Drupal.behaviors.drupalorgSearch = function () {
   $('body.page-search #content-top-region form:not(.drupalorgSearch-processed)').addClass('drupalorgSearch-processed').each(function () {
     var $this = $(this);
