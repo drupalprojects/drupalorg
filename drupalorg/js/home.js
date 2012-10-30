32b48283f4135dcb33256a3d3e65d889b8eac008
Neil Drumm <drumm@delocalizedham.com>
2012-10-30 15:12:08 -0700

    [#1820270] Convert to D7 behaviors.

--- drupalorg/js/home.js
+++ drupalorg/js/home.js
@@ -1,23 +1,27 @@
 (function ($) {
-  var current = 0,
-    $map = $('.homepage-map'),
-    $pins = $('.homepage-map>.homepage-pin');
+  Drupal.behaviors.drupalorgHome = {
+    attach: function (context, settings) {
+      var current = 0,
+        $map = $('.homepage-map', context),
+        $pins = $('.homepage-map>.homepage-pin', context);
 
-  $pins.each(function () {
-    $(this).css({
-      left: '' + Drupal.longitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
-      bottom: '' + Drupal.latitudeToPx($('>.latitude', this).text(), 78, -58, $map.height()) + 'px'
-    });
-  }).eq(current).show();
-  setInterval(function () {
-    $pins.eq(current).hide();
-    current += 1;
-    if (current === $pins.length) {
-      current = 0;
+      $pins.each(function () {
+        $(this).css({
+          left: '' + Drupal.longitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
+          bottom: '' + Drupal.latitudeToPx($('>.latitude', this).text(), 78, -58, $map.height()) + 'px'
+        });
+      }).eq(current).show();
+      if ($map.length > 0) {
+        setInterval(function () {
+          $pins.eq(current).hide();
+          current += 1;
+          if (current === $pins.length) {
+            current = 0;
+          }
+          $pins.eq(current).show();
+        }, 4000);
+      }
+      $('#community-updates', context).tabs();
     }
-    $pins.eq(current).show();
-  }, 4000);
-  $(document).ready(function() {
-    $('#community-updates').tabs();
-  });
+  };
 })(jQuery);
