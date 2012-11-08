24abd868d1532c9258805c5b11d38aa4b3b0fcb4
jhedstrom <jhedstrom@208732.no-reply.drupal.org>
2012-11-08 14:01:57 -0800

    [#1820270] Make sure drupalorg JS is included and works

--- drupalorg/js/general.js
+++ drupalorg/js/general.js
@@ -1,43 +1,51 @@
-Drupal.behaviors.drupalorgSearch = function () {
-  $('body.page-search #content-top-region form:not(.drupalorgSearch-processed)').addClass('drupalorgSearch-processed').each(function () {
-    var $this = $(this);
-    $this.find('select').change(function () {
-      $this.submit();
-    });
-  });
-};
+(function ($) {
+  Drupal.behaviors.drupalorgSearch = {
+    attach: function (context, settings) {
+      $('body.page-search #content-top-region form:not(.drupalorgSearch-processed)', context).addClass('drupalorgSearch-processed').each(function () {
+        var $this = $(this);
+        $this.find('select').change(function () {
+          $this.submit();
+        });
+      });
+    }
+  };
 
-Drupal.behaviors.drupalorgCompany = function () {
-  var $map = $('#organization-map');
-  $map.find('>.pin').each(function () {
-    $(this).css({
-      left: '' + Drupal.html5UserGeolocationLongitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
-      bottom: '' + Drupal.html5UserGeolocationLatitudeToPx($('>.latitude', this).text(), 74, -58, $map.height()) + 'px'
-    });
-  })
-};
+  Drupal.behaviors.drupalorgCompany = {
+    attach: function () {
+      var $map = $('#organization-map');
+      $map.find('>.pin').each(function () {
+        $(this).css({
+          left: '' + Drupal.html5UserGeolocationLongitudeToPx($('>.longitude', this).text(), -168, $map.width()) + 'px',
+          bottom: '' + Drupal.html5UserGeolocationLatitudeToPx($('>.latitude', this).text(), 74, -58, $map.height()) + 'px'
+        });
+      });
+    }
+  };
 
-/**
- * Marketplace listing pages.
- */
-Drupal.behaviors.drupalorgMarketplace = function () {
-  $('.view-drupalorg-organizations:not(.drupalorgMarketplace-processed)').addClass('drupalorgMarketplace-processed')
-  .find('ul').each(function () {
-    var $showMore = $('.show-more', this).hide(),
-      $showLink = $('.show-link', this),
-      $hideLink = $('.hide-link', this);
+  /**
+   * Marketplace listing pages.
+   */
+  Drupal.behaviors.drupalorgMarketplace = {
+    attach: function () {
+      $('.view-drupalorg-organizations:not(.drupalorgMarketplace-processed)').addClass('drupalorgMarketplace-processed')
+        .find('ul').each(function () {
+          var $showMore = $('.show-more', this).hide(),
+            $showLink = $('.show-link', this),
+            $hideLink = $('.hide-link', this);
 
-    $showLink.show().click(function (e) {
-      $showMore.show();
-      $showLink.hide();
-      $hideLink.show();
-      e.preventDefault();
-    });
-    $hideLink.click(function (e) {
-      $showMore.hide();
-      $showLink.show();
-      $hideLink.hide();
-      e.preventDefault();
-    });
-  });
-};
+          $showLink.show().click(function (e) {
+            $showMore.show();
+            $showLink.hide();
+            $hideLink.show();
+            e.preventDefault();
+          });
+          $hideLink.click(function (e) {
+            $showMore.hide();
+            $showLink.show();
+            $hideLink.hide();
+            e.preventDefault();
+          });
+        });
+    }
+  };
+})(jQuery);
