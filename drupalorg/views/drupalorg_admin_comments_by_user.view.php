c8b53a08e3ead09ed0683ef2b0477725d056004a
Derek Wright <git@dwwright.net>
2013-05-31 17:03:13 -0400

    [#2009032] Let 'Spam Fighter' role use the bulk comment/node delete tools

--- drupalorg/views/drupalorg_admin_comments_by_user.view.php
+++ drupalorg/views/drupalorg_admin_comments_by_user.view.php
@@ -22,6 +22,7 @@ $handler->display->display_options['access']['type'] = 'role';
 $handler->display->display_options['access']['role'] = array(
   3 => 3,
   4 => 4,
+  7 => 7,
 );
 $handler->display->display_options['cache']['type'] = 'none';
 $handler->display->display_options['query']['type'] = 'views_query';
