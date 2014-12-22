a04b2ef7e19494df128306a6cb273009af1c29ca
Neil Drumm <drumm@delocalizedham.com>
2014-12-22 00:42:09 +0000

    Issue #2389345: Change block output from .tpl.php to render array

--- drupalorg_project/critical-count-block.tpl.php
+++
@@ -1,7 +0,0 @@
-<p>Drupal 8 enters release candidate phase once there are zero critical issues.</p>
-
-  <h3><a href="/project/issues/search/drupal?status[]=1&status[]=13&status[]=8&status[]=14&status[]=4&priorities[]=400&categories[]=1&categories[]=2&version[]=8.x">
-    <em><?php print $count; ?></em> critical issues remaining</a>
-  </h3>
-
-<p>Read more about <a href="/drupal-8.0/get-involved">how to help Drupal 8</a>!</p>
