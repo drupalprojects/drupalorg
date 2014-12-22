d804aeff5ab36d05b749c321276321678924338a
Neil Drumm <drumm@delocalizedham.com>
2014-12-22 00:17:37 +0000

    Issue #2389345: Move block subject into standard place

--- drupalorg_project/critical-count-block.tpl.php
+++ drupalorg_project/critical-count-block.tpl.php
@@ -1,5 +1,3 @@
-<h2>Drupal 8 Critical Countdown</h2>
-
 <p>Drupal 8 enters release candidate phase once there are zero critical issues.</p>
 
   <h3><a href="/project/issues/search/drupal?status[]=1&status[]=13&status[]=8&status[]=14&status[]=4&priorities[]=400&categories[]=1&categories[]=2&version[]=8.x">
