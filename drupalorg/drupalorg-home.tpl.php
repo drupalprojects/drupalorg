6e72e8e020a0dc8e63fca62efbaeac7828eca475
Neil Drumm <drumm@delocalizedham.com>
2015-04-08 23:40:07 +0000

    Issue #2463205: Bring in new home page content

--- drupalorg/drupalorg-home.tpl.php
+++ drupalorg/drupalorg-home.tpl.php
@@ -1,12 +1,28 @@
-<div id="choose-drupal">
-  <h2><a href="about">Why Choose Drupal?</a></h2>
-  <p>Use Drupal to build everything from personal blogs to enterprise applications. Thousands of add-on modules and designs let you build any site you can imagine. Join us!</p>
-  <p class="front-get-started"><a href="start" class="link-button"><span>Get Started with Drupal</span></a></p>
-</div>
+<div class="front-primary">
+  <div class="call-to-action">
+    <h2><a href="/try-drupal">Try Drupal</a></h2>
+    <p>Want to quickly see how Drupal works? Try one of these solutions to get started with Drupal in minutes.</p>
+    <p class="front-get-started"><a href="/try-drupal" class="link-button"><span>Try Drupal</span></a></p>
+  </div>
 
-<div id="sites-with-drupal">
-  <h2><a href="case-studies">Sites Made with Drupal</a></h2>
-  <div class="things-we-made-wrapper"><?php print $things_we_made; ?></div>
+  <div class="call-to-action last">
+    <h2><a href="about">Get Started</a></h2>
+    <p>Use Drupal to build everything from personal blogs to enterprise applications. Thousands of add-on modules and designs let you build any site you can imagine. Join us!</p>
+    <p class="front-get-started"><a href="start" class="link-button"><span>Get Started with Drupal</span></a></p>
+  </div>
+
+  <div id="community-updates">
+    <ul>
+      <li><a href="#tab-news" id="tab-news-link">News</a></li>
+      <li><a href="#tab-docs" id="tab-docs-link"><span class="icon"></span>Docs Updates</a></li>
+      <li><a href="#tab-forum" id="tab-forum-link"><span class="icon"></span>Forum Posts</a></li>
+      <li><a href="#tab-commits" id="tab-commits-link"><span class="icon"></span>Commits</a></li>
+    </ul>
+    <div id="tab-news"><?php print $tab_content_news; ?></div>
+    <div id="tab-docs"><?php print $tab_content_docs; ?></div>
+    <div id="tab-forum"><?php print $tab_content_forums; ?></div>
+    <div id="tab-commits"><?php print $tab_content_git; ?></div>
+  </div>
 </div>
 
 <div id="develop-with-drupal">
@@ -39,15 +55,25 @@
   <?php print $psa; ?>
 </div>
 
-<div id="community-updates">
-  <ul>
-    <li><a href="#tab-news" id="tab-news-link">News</a></li>
-    <li><a href="#tab-docs" id="tab-docs-link"><span class="icon"></span>Docs Updates</a></li>
-    <li><a href="#tab-forum" id="tab-forum-link"><span class="icon"></span>Forum Posts</a></li>
-    <li><a href="#tab-commits" id="tab-commits-link"><span class="icon"></span>Commits</a></li>
-  </ul>
-  <div id="tab-news"><?php print $tab_content_news; ?></div>
-  <div id="tab-docs"><?php print $tab_content_docs; ?></div>
-  <div id="tab-forum"><?php print $tab_content_forums; ?></div>
-  <div id="tab-commits"><?php print $tab_content_git; ?></div>
+  <div class="call-to-action">
+    <h2><a href="/drupal-services">Services</a></h2>
+    <p>The Drupal community is huge. Find a company to help you build or host your site in the Drupal Marketplace.</p>
+    <p class="front-get-started"><a href="/drupal-services" class="link-button"><span>Drupal Services</span></a></p>
+  </div>
+
+  <div class="call-to-action">
+    <h2><a href="https://jobs.drupal.org/">Jobs</a></h2>
+    <p>The community is only as good as its best and brightest. Purchase a job post on Drupal Jobs to help support Drupal.org.</p>
+    <p class="front-get-started"><a href="https://jobs.drupal.org/" class="link-button"><span>Drupal Jobs</span></a></p>
+  </div>
+
+  <div class="call-to-action last">
+    <h2><a href="/supporters">Supporters</a></h2>
+    <p>Drupal Association Supporter Programs provide companies a way to give back to the Drupal project and enjoy great benefits in return.</p>
+    <p class="front-get-started"><a href="/supporters" class="link-button"><span>Drupal Supporters</span></a></p>
+  </div>
+
+<div id="sites-with-drupal">
+  <h2><a href="case-studies">Who Uses Drupal</a></h2>
+  <?php print $things_we_made; ?>
 </div>
