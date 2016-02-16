90336306d893a4f948ff80c6067042396495afbf
formatCvt <formatCvt@1853874.no-reply.drupal.org>
2016-02-16 01:30:17 +0000

    Issue #2473593 by formatC'vt: Prevent multiple submits on user side

--- drupalorg/js/general.js
+++ drupalorg/js/general.js
@@ -364,4 +364,15 @@
       });
     }
   };
+
+  /**
+   * Prevent multiple submits.
+   */
+  Drupal.behaviors.drupalorgPreventMultipleSubmit = {
+    attach: function(context, settings) {
+      if ($('form.prevent-multiple-submit-form').length) {
+        $('body').once('multisub').delegate('form.prevent-multiple-submit-form', 'submit.formSubmitSingle', $.onFormSubmitSingle);
+      }
+    }
+  };
 })(jQuery);
