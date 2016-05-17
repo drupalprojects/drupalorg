e54e4d73b59d44849e1314a35b2b6e4d81f8eccb
Neil Drumm <drumm@delocalizedham.com>
2016-05-17 14:15:44 -0400

    Remove 21bc750 NOLA featured quote block

--- drupalorg/drupalorg-home.tpl.php
+++ drupalorg/drupalorg-home.tpl.php
@@ -1,6 +1,5 @@
 <div class="front-primary">
   <div id="tab-news"><?php print $tab_content_news; ?></div>
-  <div id="membership" class="last"><?php print render($region['preface_second']); ?></div>
 </div>
 
 <div id="develop-with-drupal">
