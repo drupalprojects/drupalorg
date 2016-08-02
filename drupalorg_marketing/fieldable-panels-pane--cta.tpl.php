1e93971fbc68aadaf883b6c43c0abc7b7271a5ea
Emilie Nouveau <DyanneNova@gmail.com>
2016-08-02 11:46:18 -0700

    Add inner div on CTA fieldable panel pane

--- drupalorg_marketing/fieldable-panels-pane--cta.tpl.php
+++ drupalorg_marketing/fieldable-panels-pane--cta.tpl.php
@@ -1,8 +1,10 @@
 <div <?php if(isset($content['field_cta_background']['#items']['0']['filename'])): ?>style="background-image: url('/sites/default/files/cta/background/<?php print render($content['field_cta_background']['#items']['0']['filename']); ?>');"<?php endif; ?> class="pane-style-<?php print $content['field_cta_style']['#items'][0]['value'] . ' ' . $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($content['field_cta_graphic']); ?>
-  <div class="cta-text">
-    <h2><?php print $content['title']['#value']; ?></h2>
-    <?php print render($content['field_cta_body']); ?>
-    <?php print render($content['field_cta_link']); ?>
-  </div>
+  <div class="pane-style-<?php print $content['field_cta_style']['#items'][0]['value']; ?>-inner pane-content-inner">
+    <?php print render($content['field_cta_graphic']); ?>
+    <div class="cta-text">
+      <h2><?php print $content['title']['#value']; ?></h2>
+      <?php print render($content['field_cta_body']); ?>
+      <?php print render($content['field_cta_link']); ?>
+    </div>
+    </div>
 </div>
