7383769ab77aadc21d56c316659f4a98b59d04ee
Neil Drumm <drumm@delocalizedham.com>
2016-08-11 20:08:08 +0000

    Remove unnecessary markup

--- drupalorg/drupalorg-download.tpl.php
+++ drupalorg/drupalorg-download.tpl.php
@@ -3,9 +3,9 @@
   <div class="core">
     <h2>Core</h2>
     <ul class="flat">
-      <li class="first download-core"><?php print $core_download_button; ?></li>
+      <li class="download-core"><?php print $core_download_button; ?></li>
       <li><a href="/node/3060/release">Other Releases</a></li>
-      <li class="last all"><a class="all" href="/project/drupal">More Information</a></li>
+      <li class="all"><a href="/project/drupal">More Information</a></li>
     </ul>
   </div>
 
@@ -16,18 +16,18 @@
       <li><a href="/project/project_distribution?solrsort=iss_project_release_usage%20desc">Most Installed Distributions</a></li>
       <li><a href="/project/project_distribution?solrsort=ds_created%20desc">New Distributions</a></li>
       <li><a href="/project/project_distribution?solrsort=ds_project_latest_activity%20desc">Most Active Distributions</a></li>
-      <li class="last all"><a class="all" href="/project/project_distribution">Search for More Distributions</a></li>
+      <li class="all"><a href="/project/project_distribution">Search for More Distributions</a></li>
     </ul>
   </div>
 
   <div class="themes">
     <h2>Themes</h2>
     <ul class="flat">
-      <li class="first"><a href="node/221881">About Themes &amp; Subthemes</a></li>
+      <li><a href="node/221881">About Themes &amp; Subthemes</a></li>
       <li><a href="/project/project_theme?solrsort=iss_project_release_usage%20desc">Most Installed Themes</a></li>
       <li><a href="/project/project_theme?solrsort=ds_created%20desc">New Themes</a></li>
       <li><a href="/project/project_theme?solrsort=ds_project_latest_activity%20desc">Most Active Themes</a></li>
-      <li class="last all"><a class="all" href="/project/project_theme">Search for More Themes</a></li>
+      <li class="all"><a href="/project/project_theme">Search for More Themes</a></li>
     </ul>
   </div>
 
@@ -35,7 +35,7 @@
     <h2>Translations</h2>
     <ul class="flat">
       <?php print $drupalorg_featured_translations; ?>
-      <li class="last all"><a href="http://localize.drupal.org/" class="all">All Translations</a></li>
+      <li class="all"><a href="http://localize.drupal.org/">All Translations</a></li>
     </ul>
   </div>
 </div>
@@ -48,10 +48,6 @@
 </div>
 
 <div class="drupal-modules-facets">
-<?php /*
-  NOTE: It would be super helpful if these dynamic lists were wrapped in a <ul class="flat">
-        This class removes the padding and bullets from the list items
-*/ ?>
   <div class="most-installed">
     <?php print $iss_project_release_usage; ?>
   </div>
@@ -59,11 +55,11 @@
   <div class="categories">
     <h2>Module Categories</h2>
     <ul class="flat">
-      <li class="first"><a href="/project/project_module?f[2]=im_vid_3:53">Administration</a></li>
+      <li><a href="/project/project_module?f[2]=im_vid_3:53">Administration</a></li>
       <li><a href="/project/project_module?f[2]=im_vid_3:56">Community</a></li>
       <li><a href="/project/project_module?f[2]=im_vid_3:61">Event</a></li>
       <li><a href="/project/project_module?f[2]=im_vid_3:67">Media</a></li>
-      <li class="last all"><a href="/project/project_module/categories" class="all">All Categories</a></li>
+      <li class="all"><a href="/project/project_module/categories">All Categories</a></li>
     </ul>
   </div>
 
