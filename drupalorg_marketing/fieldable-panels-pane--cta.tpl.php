cc9bf1b7b8c79cade13a5903348b8e8a2602e395
Timothy Lehnen <tim@association.drupal.org>
2016-08-24 11:09:01 -0700

    Fixed fieldable-panels-pane--cta.tpl.php using file_create_url()

--- drupalorg_marketing/fieldable-panels-pane--cta.tpl.php
+++ drupalorg_marketing/fieldable-panels-pane--cta.tpl.php
@@ -1,4 +1,4 @@
-<div <?php if(isset($content['field_cta_background']['#items']['0']['filename'])): ?>style="background-image: url('/sites/default/files/cta/background/<?php print render($content['field_cta_background']['#items']['0']['filename']); ?>');"<?php endif; ?> class="pane-style-<?php print $content['field_cta_style']['#items'][0]['value'] . ' ' . $classes; ?>"<?php print $attributes; ?>>
+<div <?php if(isset($content['field_cta_background']['#items']['0']['filename'])): ?>style="background-image: url('<?php print file_create_url($content['field_cta_background']['#items']['0']['uri']); ?>');"<?php endif; ?> class="pane-style-<?php print $content['field_cta_style']['#items'][0]['value'] . ' ' . $classes; ?>"<?php print $attributes; ?>>
   <div class="pane-style-<?php print $content['field_cta_style']['#items'][0]['value']; ?>-inner pane-content-inner">
     <?php print render($content['field_cta_graphic']); ?>
     <div class="cta-text">
