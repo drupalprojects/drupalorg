1c55309df3bd96a47f15eb54c4c709c94dc09245
Neil Drumm <drumm@delocalizedham.com>
2016-09-22 20:20:46 +0000

    Issue #2803129: Display case studies on organization profiles (and move people to a separate link)

--- drupalorg/drupalorg-map-pin.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file HTML to plot a map pin. JS does the positioning.
- */
-?>
-<div class="<?php print $classes; ?>">
-  <span class="latitude"><?php print $latitude; ?></span>
-  <span class="longitude"><?php print $longitude; ?></span>
-</div>
