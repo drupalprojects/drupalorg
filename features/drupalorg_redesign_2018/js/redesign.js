455e8430302214f0d0b3f565bd253f5645497a58
Simona Ritrovato <simona.ritrovato@sixeleven.it>
2018-05-28 16:31:41 +0200

    Change hero animation from svg to canvas

--- features/drupalorg_redesign_2018/js/redesign.js
+++ features/drupalorg_redesign_2018/js/redesign.js
@@ -48,37 +48,71 @@
 
       // Hero animation.
       if ($('.d-hero', context).length > 0) {
-
-        for (i=0; i<6; i++) {
-          TweenMax.to("#start"+i, 6, {
-            morphSVG:"#end"+i,
-            yoyo:true,
-            repeat:-1,
-            repeatDelay: 0,
-            ease:Power2.easeInOut
-          });
+        // Canvas code.
+        var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
+        function initHero() {
+          canvas = document.getElementById("canvas");
+          anim_container = document.getElementById("animation_container");
+          dom_overlay_container = document.getElementById("dom_overlay_container");
+          var comp=AdobeAn.getComposition("4F7E56806ABB4DFCB54C9E7021D97C9F");
+          var lib=comp.getLibrary();
+          handleComplete({},comp);
+        }
+        function handleComplete(evt,comp) {
+          //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
+          var lib=comp.getLibrary();
+          var ss=comp.getSpriteSheet();
+          exportRoot = new lib.drupalhero();
+          stage = new lib.Stage(canvas);
+          //Registers the "tick" event listener.
+          fnStartAnimation = function() {
+            stage.addChild(exportRoot);
+            createjs.Ticker.setFPS(lib.properties.fps);
+            createjs.Ticker.addEventListener("tick", stage);
+          }
+          //Code to support hidpi screens and responsive scaling.
+          function makeResponsive(isResp, respDim, isScale, scaleType) {
+            var lastW, lastH, lastS=1;
+            window.addEventListener('resize', resizeCanvas);
+            resizeCanvas();
+            function resizeCanvas() {
+              var w = lib.properties.width, h = lib.properties.height;
+              var iw = window.innerWidth, ih=window.innerHeight;
+              var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;
+              if(isResp) {
+                if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {
+                  sRatio = lastS;
+                }
+                else if(!isScale) {
+                  if(iw<w || ih<h)
+                    sRatio = Math.min(xRatio, yRatio);
+                }
+                else if(scaleType==1) {
+                  sRatio = Math.min(xRatio, yRatio);
+                }
+                else if(scaleType==2) {
+                  sRatio = Math.max(xRatio, yRatio);
+                }
+              }
+              canvas.width = w*pRatio*sRatio;
+              canvas.height = h*pRatio*sRatio;
+              canvas.style.width = dom_overlay_container.style.width = anim_container.style.width =  w*sRatio+'px';
+              canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h*sRatio+'px';
+              stage.scaleX = pRatio*sRatio;
+              stage.scaleY = pRatio*sRatio;
+              lastW = iw; lastH = ih; lastS = sRatio;
+              stage.tickOnUpdate = false;
+              stage.update();
+              stage.tickOnUpdate = true;
+            }
+          }
+          makeResponsive(false,'height',false,1);
+          AdobeAn.compositionLoaded(lib.properties.id);
+          fnStartAnimation();
         }
 
-        $('#feel-the-wave').wavify({
-          height: 300,
-          bones: 5,
-          amplitude: 120,
-          speed: .15
-        });
-
-        $('#feel-the-wave-two').wavify({
-          height: 200,
-          bones: 4,
-          amplitude: 80,
-          speed: .20
-        });
-
-        $('#feel-the-wave-three').wavify({
-          height: 150,
-          bones: 4,
-          amplitude: 20,
-          speed: .25
-        });
+        // Initialize canvas.
+        initHero();
 
       }
 
